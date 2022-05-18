ec2e2e3da7a1250bfee7b4b8cb8f9cd4b711ea6f
Stéphane Caron <stephane.caron@normalesup.org>
2022-05-18 11:44:24 +0200

    Start spines/ with Upkie's servo layout

---
+++ spines/servo_layout.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 Stéphane Caron
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <vulp/actuation/ServoLayout.h>
+
+namespace upkie_locomotion::spines {
+
+/*! Get Upkie's servo layout
+ *
+ * \return Upkie's servo layout.
+ */
+inline const vulp::actuation::ServoLayout get_servo_layout() noexcept {
+  vulp::actuation::ServoLayout layout;
+  layout.add_servo(1, 1, "right_hip");
+  layout.add_servo(2, 1, "right_knee");
+  layout.add_servo(3, 1, "right_wheel");
+  layout.add_servo(4, 2, "left_hip");
+  layout.add_servo(5, 2, "left_knee");
+  layout.add_servo(6, 2, "left_wheel");
+  return layout;
+}
+
+}  // namespace upkie_locomotion::spines
