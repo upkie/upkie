acf724d126ab277a7267ac27030a851fd495ffec
Stéphane Caron <stephane.caron@normalesup.org>
2022-05-25 09:59:48 +0200

    Move spine configuration to a YAML file

--- agents/pink_balancer/main.py
+++ agents/pink_balancer/main.py
@@ -24,77 +24,10 @@ from typing import Any, Dict
 
 import aiorate
 import gin
+import yaml
 from agents.pink_balancer.whole_body_controller import WholeBodyController
 from utils.realtime import configure_cpu
 from vulp.spine import SpineInterface
-
-config = {
-    "bullet": {
-        "control_mode": "torque",
-        "follower_camera": False,
-        "gui": True,
-        "position_init_base_in_world": [0.0, 0.0, 0.6],
-        "torque_control": {
-            "kp": 20.0,
-            "kd": 1.0,
-        },
-    },
-    "cpu": 3,
-    "floor_contact": {
-        "upper_leg_torque_threshold": 10.0,
-    },
-    "servo_layout": {
-        "left_hip": {
-            "bus": 2,
-            "configuration_index": 7,
-            "id": 4,
-        },
-        "left_knee": {
-            "bus": 2,
-            "configuration_index": 8,
-            "id": 5,
-        },
-        "left_wheel": {
-            "bus": 2,
-            "id": 6,
-        },
-        "right_hip": {
-            "bus": 1,
-            "configuration_index": 10,
-            "id": 1,
-        },
-        "right_knee": {
-            "bus": 1,
-            "configuration_index": 11,
-            "id": 2,
-        },
-        "right_wheel": {
-            "bus": 1,
-            "id": 3,
-        },
-    },
-    "upkie": {
-        "brain": {
-            "joystick": {
-                "crouch_velocity": 0.02,
-            },
-        },
-        "height": 0.30,
-    },
-    "wheel_contact": {
-        "cutoff_period": 0.2,
-        "liftoff_inertia": 0.001,
-        "min_touchdown_acceleration": 2.0,
-        "min_touchdown_torque": 0.015,
-        "touchdown_inertia": 0.004,
-    },
-    "wheel_odometry": {
-        "signed_radius": {
-            "left_wheel": +0.06,
-            "right_wheel": -0.06,
-        },
-    },
-}
 
 
 def parse_command_line_arguments() -> argparse.Namespace:
@@ -145,20 +78,25 @@ async def run(
 
 if __name__ == "__main__":
     args = parse_command_line_arguments()
-
     agent_dir = path.dirname(__file__)
     blue_balancer = path.join(agent_dir, "../blue_balancer")
+
+    # Gin configuration
     gin.parse_config_file(f"{agent_dir}/whole_body_controller.gin")
     gin.parse_config_file(f"{blue_balancer}/wheel_balancer.gin")
     if args.config == "default":
-        logging.warn('No configuration specified, assuming "bullet"')
+        logging.warning('No configuration specified, assuming "bullet"')
         args.config = "bullet"
     if args.config == "pi3hat":
         gin.parse_config_file(f"{blue_balancer}/pi3hat.gin")
     elif args.config == "bullet":
         gin.parse_config_file(f"{blue_balancer}/bullet.gin")
+
+    # Spine configuration
+    with open(f"{blue_balancer}/spine.yaml", "r") as fh:
+        config = yaml.safe_load(fh)
     if args.config == "pi3hat":
-        configure_cpu(config["cpu"])
+        configure_cpu(cpu=3)
 
     spine = SpineInterface()
     try:
