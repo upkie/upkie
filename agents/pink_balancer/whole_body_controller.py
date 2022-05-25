548f7bf5f4c237e44e8c246ceb4de4aa6bd40dde
Stéphane Caron <stephane.caron@normalesup.org>
2022-05-25 08:45:15 +0200

    WIP: Configuration vector from observation dict

---
+++ agents/pink_balancer/whole_body_controller.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Dict
+
+import gin
+import numpy as np
+import pink
+import pink.models
+import pinocchio as pin
+import upkie_description
+from agents.pink_balancer.wheel_balancer import WheelBalancer
+from pink import solve_ik
+from pink.tasks import BodyTask, PostureTask
+from pink.utils import custom_configuration_vector
+from utils.clamp import clamp
+
+
+def observe(observation, configuration, servo_layout) -> np.ndarray:
+    """
+    Compute configuration vector from a new observation.
+
+    Args:
+        observation: Observation dictionary.
+        configuration: Previous configuration.
+        servo_layout: Robot servo layout.
+
+    Returns:
+        Configuration vector from observation.
+    """
+    q = configuration.q.copy()
+    for joint, servo in servo_layout.items():
+        if "configuration_index" not in servo:
+            continue
+        i = servo["configuration_index"]
+        q[i] = observation["servo"][joint]["position"]
+        # tangent_index = configuration_index - 1
+        # v[tangent_index] = observation["servo"][joint]["velocity"]
+    return q
