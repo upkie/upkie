ead8cdfa02f792543c792eeeeb8324ee5d6ffafe
Stéphane Caron <stephane.caron@normalesup.org>
2023-01-27 10:25:44 +0100

    [pink_balancer] Move config files to subdirectory

--- agents/pink_balancer/pink_balancer.py
+++ agents/pink_balancer/pink_balancer.py
@@ -116,15 +116,14 @@ if __name__ == "__main__":
     agent_dir = path.dirname(__file__)
 
     # Gin configuration
-    gin.parse_config_file(f"{agent_dir}/whole_body_controller.gin")
-    gin.parse_config_file(f"{agent_dir}/wheel_balancer.gin")
+    gin.parse_config_file(f"{agent_dir}/config/common.gin")
     if args.config == "pi3hat":
-        gin.parse_config_file(f"{agent_dir}/pi3hat.gin")
+        gin.parse_config_file(f"{agent_dir}/config/pi3hat.gin")
     elif args.config == "bullet":
-        gin.parse_config_file(f"{agent_dir}/bullet.gin")
+        gin.parse_config_file(f"{agent_dir}/config/bullet.gin")
 
     # Spine configuration
-    with open(f"{agent_dir}/spine.yaml", "r") as fh:
+    with open(f"{agent_dir}/config/spine.yaml", "r") as fh:
         config = yaml.safe_load(fh)
     if args.config == "pi3hat":
         configure_cpu(cpu=3)
