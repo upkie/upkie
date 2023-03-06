8cf5d367c5cba0f0431373e35ea14ed6ff38c077
Stéphane Caron <stephane.caron@normalesup.org>
2023-03-06 15:46:48 +0100

    [minor] Black behavior changed a tad

--- observers/base_pitch/base_pitch.py
+++ observers/base_pitch/base_pitch.py
@@ -95,19 +95,19 @@ def rotation_matrix_from_quaternion(
     return np.array(
         [
             [
-                1 - 2 * (qy ** 2 + qz ** 2),
+                1 - 2 * (qy**2 + qz**2),
                 2 * (qx * qy - qz * qw),
                 2 * (qw * qy + qx * qz),
             ],
             [
                 2 * (qx * qy + qz * qw),
-                1 - 2 * (qx ** 2 + qz ** 2),
+                1 - 2 * (qx**2 + qz**2),
                 2 * (qy * qz - qx * qw),
             ],
             [
                 2 * (qx * qz - qy * qw),
                 2 * (qy * qz + qx * qw),
-                1 - 2 * (qx ** 2 + qy ** 2),
+                1 - 2 * (qx**2 + qy**2),
             ],
         ]
     )
