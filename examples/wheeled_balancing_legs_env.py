ccdc9ca0a5b8c88c0bef5a0ff3dea73cf2401c68
Stéphane Caron <stephane.caron@normalesup.org>
2023-04-04 15:38:13 +0200

    Fix wheeled balancing in legs env

--- examples/wheeled_balancing_legs_env.py
+++ examples/wheeled_balancing_legs_env.py
@@ -4,6 +4,8 @@
 # Copyright 2023 Inria
 
 import gym
+import numpy as np
+
 import upkie_locomotion.envs
 
 if __name__ == "__main__":
@@ -11,13 +13,15 @@ if __name__ == "__main__":
     env = gym.make("UpkieLegsEnv-v1")
     observation = env.reset(seed=42)
 
-    action = env.action_space.sample()
+    action = 0.0 * env.action_space.sample()
     for step in range(1_000_000):
         observation, reward, done, _ = env.step(action)
         if done:
             observation = env.reset()
         pitch = observation[0]
-        action[2] = 10.0 * pitch
-        action[5] = 10.0 * pitch
+        action[2] = np.nan
+        action[6 + 2] = 200.0 * pitch
+        action[5] = np.nan
+        action[6 + 5] = -200.0 * pitch
 
     env.close()
