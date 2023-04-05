e33b19c009a9873c7b517f9db21a572a070afd52
Stéphane Caron <stephane.caron@normalesup.org>
2023-04-05 17:45:00 +0200

    Add new clamping function and unit tests

--- utils/clamp.py
+++ utils/clamp.py
@@ -17,6 +17,8 @@
 
 from typing import Optional
 
+from .spdlog import logging
+
 
 def clamp(
     value: float, lower: Optional[float] = None, upper: Optional[float] = None
@@ -45,3 +47,23 @@ def clamp_abs(value: float, bound: float):
         bound: Absolute value bound.
     """
     return clamp(value, -bound, bound)
+
+
+def clamp_and_warn(value: float, lower: float, upper: float, label: str):
+    """
+    Clamp a value between a lower and an upper bound, warning if the value is
+    changed.
+
+    Args:
+        value: Value to clamp.
+        lower: Lower bound.
+        upper: Upper bound.
+        label: Label to describe the value.
+    """
+    if value < lower:
+        logging.warning(f"{label}={value} clamped to {lower=}")
+        return lower
+    elif value > upper:
+        logging.warning(f"{label}={value} clamped to {upper=}")
+        return upper
+    return value
