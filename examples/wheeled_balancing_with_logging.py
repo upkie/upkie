958179ab51896eef03039299ef7ac35626141f09
Stéphane Caron <stephane.caron@normalesup.org>
2023-04-13 19:16:11 +0200

    Add example with logging

---
+++ examples/wheeled_balancing_with_logging.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+
+"""Wheel balancing with time-series logging of observations and actions."""
+
+import asyncio
+import time
+
+import gym
+import mpacklog
+import numpy as np
+from loop_rate_limiters import AsyncRateLimiter
+
+import upkie_locomotion.envs
+
+upkie_locomotion.envs.register()
+
+
+async def balance(env: gym.Env, logger: mpacklog.Logger):
+    """Run proportional balancer in gym environment with logging."""
+    observation = env.reset()  # connects to the spine
+    action = np.zeros(env.action_space.shape)
+    rate = AsyncRateLimiter(frequency=200.0)
+    for step in range(1_000_000):
+        await rate.sleep()
+        observation, reward, done, info = env.step(action)
+        if done:
+            observation = env.reset()
+        pitch = observation[0]
+        action[0] = 10.0 * pitch
+        await logger.put(  # log info to be written to file later
+            {
+                "action": info["action"],
+                "observation": info["observation"],
+                "policy": {
+                    "action": action,
+                    "observation": observation,
+                },
+                "time": time.time(),
+            }
+        )
+
+
+async def main():
+    """Main function of our asyncio program."""
+    env = gym.make("UpkieWheelsEnv-v2")
+    logger = mpacklog.Logger("wheeled_balancing.mpack")
+    try:
+        await asyncio.gather(
+            balance(env, logger),
+            logger.write(),  # write logs to file when there is time
+        )
+    finally:
+        env.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
