0ecd18bf3cf9718aa1257311d353be942971c34d
Stéphane Caron <stephane.caron@normalesup.org>
2023-04-13 18:46:18 +0200

    Add loop rate limiter to wheeled_balancing example

--- examples/wheeled_balancing.py
+++ examples/wheeled_balancing.py
@@ -5,22 +5,29 @@
 
 import gym
 import numpy as np
+from loop_rate_limiters import RateLimiter
 
 import upkie_locomotion.envs
 
 upkie_locomotion.envs.register()
 
+
+def balance(env: gym.Env):
+    observation = env.reset()  # connects to the spine
+    action = np.zeros(env.action_space.shape)
+    rate = RateLimiter(frequency=400.0)
+    for step in range(1_000_000):
+        observation, reward, done, _ = env.step(action)
+        if done:
+            observation = env.reset()
+        pitch = observation[0]
+        action[0] = 10.0 * pitch
+        rate.sleep()
+
+
 if __name__ == "__main__":
     env = gym.make("UpkieWheelsEnv-v2")
-
     try:
-        observation = env.reset(seed=42)  # connects to the spine
-        action = np.zeros(env.action_space.shape)
-        for step in range(1_000_000):
-            observation, reward, done, _ = env.step(action)
-            if done:
-                observation = env.reset()
-            pitch = observation[0]
-            action[0] = 10.0 * pitch
+        balance(env)
     finally:  # make sure we disconnect from the spine
         env.close()
