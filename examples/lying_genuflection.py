2a8157e74c98ef24785ac2cfc27e107d4bcaa202
Stéphane Caron <stephane.caron@normalesup.org>
2023-06-06 15:55:44 +0200

    Add lying genuflection example

--- examples/lying_genuflection.py
+++ examples/lying_genuflection.py
@@ -12,20 +12,29 @@ from loop_rate_limiters import RateLimiter
 import upkie.envs
 from upkie.observers.base_pitch import compute_base_pitch_from_imu
 
+nb_genuflections = 10
+genuflection_steps = 200
+amplitude = 2.0  # in radians
+
+config = {
+    "bullet": {
+        "orientation_init_base_in_world": [0.707, 0.0, -0.707, 0.0],
+        "position_init_base_in_world": [0.0, 0.0, 0.1],
+    }
+}
+
 if __name__ == "__main__":
     upkie.envs.register()
-    with gym.make("UpkieServosEnv-v1") as env:
-        observation = env.reset()  # connects to the spine
+    with gym.make("UpkieServosEnv-v1", config=config) as env:
+        observation = env.reset()
         action = np.zeros(env.action_space.shape)
         rate = RateLimiter(frequency=200.0)
-        for step in range(1_000_000):
+        for step in range(nb_genuflections * genuflection_steps):
             observation, reward, done, info = env.step(action)
             imu = info["observation"]["imu"]
-            if done:
-                env.reset()
             pitch = compute_base_pitch_from_imu(imu["orientation"])
-            action[2] = np.nan  # no position target for left wheel
-            action[5] = np.nan  # same for right wheel
-            action[6 + 2] = 200.0 * pitch
-            action[6 + 5] = -200.0 * pitch
+            x = float(step % genuflection_steps) / genuflection_steps
+            y = 2.0 * x * (1.0 - x)  # in [0, 1]
+            A = amplitude  # in radians
+            action[[0, 1, 3, 4]] = A * y * np.array([1.0, -2.0, -1.0, 2.0])
             rate.sleep()
