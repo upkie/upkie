94756465912bc1641b94e1f326347004aadbebb1
Stéphane Caron <stephane.caron@normalesup.org>
2023-06-15 11:50:58 +0200

    WIP: Add setup script to configure a new Raspberry Pi
    This is the host-side script

---
+++ tools/setup/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Copyright 2022 Stéphane Caron
+# Copyright 2023 Inria
+#
+# Run this script from the host to configure a target Raspberry Pi system.
+
+set -e
+
+if [ $# -ne 6 ]
+then
+    echo "Usage: $0 <[user@]host> <ssid> <wifi-password> <country> <wifi-prefix> <eth-prefix>"
+    exit
+fi
+
+CURDIR=$(dirname $0)
+ROBOT=${1}  # [user@]hostname of target platform
+SSID=${2}
+WIFI_PASSWORD=${3}
+COUNTRY=${4}
+WIFI_PREFIX=${5}
+ETH_PREFIX=${6}
+
+scp -r ${CURDIR}/scripts ${ROBOT}:scripts
+ssh ${ROBOT} sudo ./scripts/setup-system.py ${SSID} ${WIFI_PASSWORD} ${COUNTRY} ${WIFI_PREFIX} ${ETH_PREFIX}
