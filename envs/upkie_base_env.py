59f3208dd0773de9258fd0e9cab7a0bc939326dc
Stéphane Caron <stephane.caron@normalesup.org>
2023-06-29 15:10:55 +0200

    Add missing observer configuration in base Upkie environment

--- envs/upkie_base_env.py
+++ envs/upkie_base_env.py
@@ -36,7 +36,17 @@ DEFAULT_CONFIG = {
             "kp": 20.0,
             "kd": 1.0,
         },
-    }
+    },
+    "floor_contact": {
+        "upper_leg_torque_threshold": 10.0,
+    },
+    "wheel_contact": {
+        "cutoff_period": 0.2,
+        "liftoff_inertia": 0.001,
+        "min_touchdown_acceleration": 2.0,
+        "min_touchdown_torque": 0.015,
+        "touchdown_inertia": 0.004,
+    },
 }
 
 
