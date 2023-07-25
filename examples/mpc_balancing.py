53e313d95b598e4fe1ccd930b5dd355f3ee1d1c2
Stéphane Caron <stephane.caron@normalesup.org>
2023-07-25 14:19:46 +0200

    Update MPC example to the latest cart-pole API

--- examples/mpc_balancing.py
+++ examples/mpc_balancing.py
@@ -22,7 +22,7 @@ upkie.envs.register()
 
 
 def get_target_states(
-    params: CartPole.Parameters, state: np.ndarray, target_vel: float
+    cart_pole: CartPole, state: np.ndarray, target_vel: float
 ):
     """Define the reference state trajectory over the receding horizon.
 
@@ -34,9 +34,9 @@ def get_target_states(
         Goal state at the end of the horizon.
     """
     nx = CartPole.STATE_DIM
-    T = params.sampling_period
-    target_states = np.zeros((params.nb_timesteps + 1) * nx)
-    for k in range(params.nb_timesteps + 1):
+    T = cart_pole.sampling_period
+    target_states = np.zeros((cart_pole.nb_timesteps + 1) * nx)
+    for k in range(cart_pole.nb_timesteps + 1):
         target_states[k * nx] = state[0] + (k * T) * target_vel
         target_states[k * nx + 2] = target_vel
     return target_states
@@ -49,20 +49,23 @@ async def balance(env: gym.Env, logger: mpacklog.AsyncLogger):
     @param env Gym environment to Upkie.
     @param logger Additional logger.
     """
-    cart_pole_params = CartPole.Parameters(
+    cart_pole = CartPole(
         length=0.4,
         max_ground_accel=10.0,
         nb_timesteps=5,
         sampling_period=0.05,
     )
-    mpc_problem = CartPole.build_mpc_problem(
-        cart_pole_params,
+    mpc_problem = cart_pole.build_mpc_problem(
         terminal_cost_weight=10.0,
         stage_state_cost_weight=1.0,
         stage_input_cost_weight=1e-3,
     )
-    cart_pole = CartPole(cart_pole_params, initial_state=np.zeros(4))
-    cart_pole.init_live_plot(order="velocities")
+
+    live_plot = None
+    if False:
+        from ltv_mpc.live_plots import CartPolePlot  # imports matplotlib
+
+        live_plot = CartPolePlot(cart_pole, order="velocities")
 
     env.reset()  # connects to the spine
     action = np.zeros(env.action_space.shape)
@@ -94,22 +97,21 @@ async def balance(env: gym.Env, logger: mpacklog.AsyncLogger):
             ]
         )
         target_vel = 0.0
-        target_states = get_target_states(
-            cart_pole_params, initial_state, target_vel
-        )
+        target_states = get_target_states(cart_pole, initial_state, target_vel)
 
         mpc_problem.update_initial_state(initial_state)
         mpc_problem.update_goal_state(target_states[-CartPole.STATE_DIM :])
         mpc_problem.update_target_states(target_states[: -CartPole.STATE_DIM])
 
         plan = solve_mpc(mpc_problem, solver="proxqp")
-        t = time.time()
         if plan.is_empty:
             logging.error("Solver found no solution to the MPC problem")
             logging.info("Continuing with last action")
-        else:
+        else:  # plan was found
             cart_pole.state = initial_state
-            cart_pole.update_live_plot(plan, t, t)
+            if live_plot is not None:
+                t = time.time()
+                live_plot.update(plan, t, initial_state, t)
             commanded_accel = plan.first_input
             commanded_velocity = clamp_and_warn(
                 commanded_velocity + commanded_accel * env.dt,
@@ -122,7 +124,7 @@ async def balance(env: gym.Env, logger: mpacklog.AsyncLogger):
             {
                 "action": info["action"],
                 "observation": info["observation"],
-                "time": t,
+                "time": time.time(),
             }
         )
     await logger.stop()
