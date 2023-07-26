494ecf7a77e1cf4d3d60e0f6653b367def2ea557
Stéphane Caron <stephane.caron@normalesup.org>
2023-07-26 14:05:27 +0200

    utils: Configure agent process on the Raspberry Pi

--- utils/raspi.py
+++ utils/raspi.py
@@ -16,15 +16,33 @@
 # limitations under the License.
 
 import os
+import sys
+
+from .spdlog import logging
+
+__AGENT_CPUID: int = 3
+__MODEL_PATH: str = "/sys/firmware/devicetree/base/model"
+__ON_RASPI: bool = False
+
+if os.path.exists(__MODEL_PATH):
+    with open(__MODEL_PATH, "r", encoding="utf-8") as fh:
+        line = fh.readline()
+        __ON_RASPI = line.startswith("Raspberry")
 
 
 def on_raspi() -> bool:
     """!
     Check whether we are running on a Raspberry Pi.
     """
-    model_path = "/sys/firmware/devicetree/base/model"
-    if not os.path.exists(model_path):
-        return False
-    with open(model_path, "r", encoding="utf-8") as fh:
-        line = fh.readline()
-    return line.startswith("Raspberry")
+    return __ON_RASPI
+
+
+def configure_agent_process() -> None:
+    """!
+    Configure a process to run as an agent on the Raspberry Pi.
+    """
+    if os.geteuid() != 0:
+        logging.info("Re-running as root to set the CPU affinity")
+        args = ["sudo", "-E", sys.executable] + sys.argv + [os.environ]
+        os.execlpe("sudo", *args)
+    os.sched_setaffinity(0, {__AGENT_CPUID})
