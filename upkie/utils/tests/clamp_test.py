3fdb36408cc6f2ace6a11c01e88016881f990a03
Stéphane Caron <stephane.caron@normalesup.org>
2023-08-31 18:39:27 +0200

    [utils] Add unit test for clamping functions

--- upkie/utils/tests/clamp_test.py
+++ upkie/utils/tests/clamp_test.py
@@ -15,9 +15,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test Pinocchio utility functions."""
+"""Test clamping utility functions."""
 
 import unittest
+
+import numpy as np
 
 from upkie.utils.clamp import clamp, clamp_abs, clamp_and_warn
 
@@ -32,8 +34,12 @@ class TestClamp(unittest.TestCase):
         self.assertAlmostEqual(clamp_abs(3.1, 2.0), 2.0)
 
     def test_clamp_and_warn(self):
-        self.assertAlmostEqual(clamp_and_warn(-2.1, -2., 2.0, "x"), -2.0)
+        self.assertAlmostEqual(clamp_and_warn(-2.1, -2.0, 2.0, "x"), -2.0)
         self.assertAlmostEqual(clamp_and_warn(3.1, -2.0, 2.0, "x"), 2.0)
+
+    def test_clamp_inf(self, x=1.42):
+        self.assertAlmostEqual(clamp(x, -np.inf, np.inf), x)
+        self.assertAlmostEqual(clamp_abs(x, np.inf), x)
 
 
 if __name__ == "__main__":
