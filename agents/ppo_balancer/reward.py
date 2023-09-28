d18d5a90ee7d1877b515f11989ae907294d9fadb
Stéphane Caron <stephane.caron@normalesup.org>
2023-09-28 12:10:36 +0200

    [ppo_balancer] WIP: Play with reward

--- agents/ppo_balancer/reward.py
+++ agents/ppo_balancer/reward.py
@@ -26,52 +26,13 @@ class Reward(upkie.envs.Reward):
 
     """!
     Reward function for balancing in place.
-
-    This class has the following attributes:
-
-    - ``lookahead_duration``: Length of the receding horizon, used to compute
-      an internal divergent component of motion.
-    - ``max_ground_accel``: Maximum ground acceleration in m/s^2.
-    - ``max_pitch``: Maximum pitch angle we expect to observe, in [rad].
-    - ``max_position``: Maximum ground position we expect to observe, in [m].
-    - ``pitch_weight``: Weight of the pitch objective in the reward.
-    - ``position_weight``: Weight of the position objective in the reward.
     """
 
-    lookahead_duration: float
-    max_ground_accel: float
-    max_pitch: float
-    max_position: float
-    pitch_weight: float
-    position_weight: float
-
-    def __init__(
-        self,
-        action_weight: float,
-        lookahead_duration: float,
-        max_action: float,
-        max_pitch: float,
-        max_position: float,
-        pitch_weight: float,
-        position_weight: float,
-    ):
+    def __init__(self, low_acceleration_weight: float):
         """!
         Initialize reward.
-
-        @param lookahead_duration Length of the receding horizon, used to
-            compute an internal divergent component of motion.
-        @param max_pitch Maximum pitch angle for standardization, in [rad].
-        @param max_position Large ground position for standardization, in [m].
-        @param pitch_weight Weight of the pitch objective in the reward.
-        @param position_weight Weight of the position objective in the reward.
         """
-        self.action_weight = action_weight
-        self.lookahead_duration = lookahead_duration
-        self.max_action = max_action
-        self.max_pitch = max_pitch
-        self.max_position = max_position
-        self.pitch_weight = pitch_weight
-        self.position_weight = position_weight
+        self.low_acceleration_weight = low_acceleration_weight
 
     def get(self, observation: np.ndarray, action: np.ndarray) -> float:
         """!
@@ -81,20 +42,16 @@ class Reward(upkie.envs.Reward):
         @param action Action to base the reward on.
         @returns Reward earned from executing the action from the observation.
         """
-        pitch = observation[0]
-        ground_position = observation[1]
+        # pitch = observation[0]
+        # ground_position = observation[1]
         ground_velocity = observation[2]
-        angular_velocity = observation[3]
+        # abs_angular_velocity = abs(observation[3])
 
-        T = self.lookahead_duration
-        lookahead_pitch = pitch + T * angular_velocity
-        lookahead_position = ground_position + T * ground_velocity
-        normalized_lookahead_pitch = lookahead_pitch / self.max_pitch
-        normalized_lookahead_position = lookahead_position / self.max_position
-        normalized_action = action[0] / self.max_action
-        return (
-            1.0
-            - self.pitch_weight * abs(normalized_lookahead_pitch)
-            - self.position_weight * abs(normalized_lookahead_position)
-            - self.action_weight * normalized_action
+        commanded_velocity = action[0]
+
+        # low_velocity = np.exp(-1.5 * ground_velocity**2)
+        # low_velocity = -(ground_velocity**2)
+        low_acceleration = np.exp(
+            -1.5 * (commanded_velocity - ground_velocity) ** 2
         )
+        return 1.0 + self.low_acceleration_weight * low_acceleration
