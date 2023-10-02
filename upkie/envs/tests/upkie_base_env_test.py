9fb0b7a67924d9b7cabdcad07568ecf948f5a7a6
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-02 16:21:29 +0200

    Unit test to cover lack of nested config update

--- upkie/envs/tests/upkie_base_env_test.py
+++ upkie/envs/tests/upkie_base_env_test.py
@@ -58,6 +58,24 @@ class TestUpkieBaseEnv(unittest.TestCase):
         observation_dict = info["observation"]
         self.assertGreaterEqual(observation_dict["number"], 1)
 
+    def test_spine_config(self):
+        shm_name = "/vroum"
+        shared_memory = posix_ipc.SharedMemory(
+            shm_name, posix_ipc.O_RDWR | posix_ipc.O_CREAT, size=42
+        )
+        env = UpkieBaseChild(
+            reward=Reward(),
+            fall_pitch=1.0,
+            frequency=100.0,
+            shm_name=shm_name,
+            spine_config={"some_value": 12, "bullet": {"gui": False}},
+        )
+        shared_memory.close_fd()
+        self.assertEqual(env.spine_config["some_value"], 12)
+        self.assertEqual(env.spine_config["some_value"], 12)
+        self.assertEqual(env.spine_config["bullet"]["control_mode"], "torque")
+        self.assertEqual(env.spine_config["bullet"]["gui"], False)
+
 
 if __name__ == "__main__":
     unittest.main()
