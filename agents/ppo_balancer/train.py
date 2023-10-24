5059d3eb179bd22d0a7858c4ce36d8db49d272c6
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-24 16:21:24 +0200

    [minor] Rename learning rate steps

--- agents/ppo_balancer/train.py
+++ agents/ppo_balancer/train.py
@@ -279,7 +279,7 @@ def train_policy(
         vec_env,
         learning_rate=exponential_decay_schedule(
             ppo_settings.learning_rate,
-            nb_steps=ppo_settings.nb_lr_schedule_steps,
+            nb_steps=ppo_settings.learning_rate_steps,
         ),
         n_steps=ppo_settings.n_steps,
         batch_size=ppo_settings.batch_size,
