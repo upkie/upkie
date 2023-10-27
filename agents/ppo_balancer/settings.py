754280d8407488303ea0b67e373a1595da8288c9
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-27 18:17:44 +0200

    Try out SAC but for some reason fps is low
    With 3 parallel envs, fps is around 150, compared to 1000+ on the same
    CPU with PPO.

--- agents/ppo_balancer/settings.py
+++ agents/ppo_balancer/settings.py
@@ -43,7 +43,6 @@ class PPOSettings:
     ent_coef: float
     gae_lambda: float
     learning_rate: float
-    learning_rate_phases: int
     max_grad_norm: float
     n_epochs: int
     n_steps: int
@@ -54,3 +53,28 @@ class PPOSettings:
     target_kl: Optional[float]
     use_sde: bool
     vf_coef: float
+
+
+@gin.configurable
+@dataclass
+class SACSettings:
+    """SAC algorithm settings."""
+
+    action_noise: Optional[float]
+    batch_size: int
+    buffer_size: int
+    ent_coef: str
+    gradient_steps: int
+    learning_rate: float
+    learning_starts: int
+    net_arch_pi: Tuple[int, int]
+    net_arch_qf: Tuple[int, int]
+    optimize_memory_usage: bool
+    sde_sample_freq: int
+    stats_window_size: int
+    target_entropy: str
+    target_update_interval: int
+    tau: float
+    train_freq: int
+    use_sde: bool
+    use_sde_at_warmup: bool
