846ff39622cf261443f3d91cfeb643cf94bdee18
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-30 08:01:18 +0100

    Make action noiser a gymnasium.ActionWrapper

--- agents/ppo_balancer/wrappers.py
+++ agents/ppo_balancer/wrappers.py
@@ -6,20 +6,24 @@
 
 import gymnasium
 import numpy as np
+from gymnasium import spaces
 
 
-class ActionNoiser(gymnasium.Wrapper):
+class ActionNoiser(gymnasium.ActionWrapper):
     def __init__(self, env, noise: np.ndarray):
+        super(ActionNoiser, self).__init__(env)
         self.high = +np.abs(noise)
         self.low = -np.abs(noise)
-        super(ActionNoiser, self).__init__(env)
+        self.action_space = spaces.Box(
+            low=env.action_space.low + self.low,
+            high=env.action_space.high + self.high,
+            shape=env.action_space.shape,
+            dtype=env.action_space.dtype,
+        )
 
-    def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
-
-    def step(self, action):
+    def action(self, action):
         noise = self.np_random.uniform(low=self.low, high=self.high)
-        return self.env.step(action + noise)
+        return action + noise
 
 
 class ObservationNoiser(gymnasium.Wrapper):
