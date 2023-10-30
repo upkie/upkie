8b8c318246cc840f299d41ea202001eb16a1f198
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-30 17:41:15 +0100

    [envs] Unit test with SB3 checker on base environment

--- upkie/envs/tests/upkie_base_env_test.py
+++ upkie/envs/tests/upkie_base_env_test.py
@@ -10,21 +10,43 @@ import unittest
 
 import numpy as np
 import posix_ipc
+from gymnasium import spaces
 from numpy.typing import NDArray
+from stable_baselines3.common.env_checker import check_env
 
 from upkie.envs import UpkieBaseEnv
 from upkie.envs.tests.mock_spine import MockSpine
 
 
-class UpkieBaseChild(UpkieBaseEnv):
+class UpkieTestEnv(UpkieBaseEnv):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.observation_space = spaces.Box(
+            -1.0,
+            +1.0,
+            shape=(1,),
+            dtype=np.float32,
+        )
+        self.action_space = spaces.Box(
+            -1.0,
+            1.0,
+            shape=(1,),
+            dtype=np.float32,
+        )
+
     def parse_first_observation(self, observation_dict: dict) -> None:
         pass
 
     def vectorize_observation(self, observation_dict: dict) -> NDArray[float]:
-        return np.empty(42)
+        return np.full((1,), 0.5, dtype=self.observation_space.dtype)
 
     def dictionarize_action(self, action: NDArray[float]) -> dict:
-        return {}
+        return {"test": action}
+
+    def get_reward(
+        self, observation: NDArray[float], action: NDArray[float]
+    ) -> float:
+        return 1.0
 
 
 class TestUpkieBaseEnv(unittest.TestCase):
@@ -33,7 +55,7 @@ class TestUpkieBaseEnv(unittest.TestCase):
         shared_memory = posix_ipc.SharedMemory(
             shm_name, posix_ipc.O_RDWR | posix_ipc.O_CREAT, size=42
         )
-        self.env = UpkieBaseChild(
+        self.env = UpkieTestEnv(
             fall_pitch=1.0,
             frequency=100.0,
             shm_name=shm_name,
@@ -53,7 +75,7 @@ class TestUpkieBaseEnv(unittest.TestCase):
         shared_memory = posix_ipc.SharedMemory(
             shm_name, posix_ipc.O_RDWR | posix_ipc.O_CREAT, size=42
         )
-        env = UpkieBaseChild(
+        env = UpkieTestEnv(
             fall_pitch=1.0,
             frequency=100.0,
             shm_name=shm_name,
@@ -63,6 +85,9 @@ class TestUpkieBaseEnv(unittest.TestCase):
         self.assertEqual(env._spine_config["some_value"], 12)
         self.assertEqual(env._spine_config["some_value"], 12)
         self.assertEqual(env._spine_config["bullet"]["gui"], False)
+
+    def test_check_env(self):
+        check_env(self.env)
 
 
 if __name__ == "__main__":
