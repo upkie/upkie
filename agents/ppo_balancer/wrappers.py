e1be88bdbbe9943b8c1a2484d2918ea51344161f
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-30 07:51:34 +0100

    Replace custom ActionNormalizer by gymnasium's RescaleAction
    See https://gymnasium.farama.org/api/wrappers/action_wrappers/#gymnasium.wrappers.RescaleAction

--- agents/ppo_balancer/wrappers.py
+++ agents/ppo_balancer/wrappers.py
@@ -22,27 +22,6 @@ class ActionNoiser(gymnasium.Wrapper):
         return self.env.step(action + noise)
 
 
-class ActionNormalizer(gymnasium.Wrapper):
-    def __init__(self, env):
-        assert isinstance(env.action_space, gymnasium.spaces.Box)
-        self.low = env.action_space.low
-        self.delta = env.action_space.high - env.action_space.low
-        env.action_space = gymnasium.spaces.Box(
-            low=-1.0,
-            high=1.0,
-            shape=env.action_space.shape,
-            dtype=np.float32,
-        )
-        super(ActionNormalizer, self).__init__(env)
-
-    def reset(self, **kwargs):
-        return self.env.reset(**kwargs)
-
-    def step(self, action):
-        rescaled_action = self.low + 0.5 * (1.0 + action) * self.delta
-        return self.env.step(rescaled_action)
-
-
 class ObservationNoiser(gymnasium.Wrapper):
     def __init__(self, env, noise: np.ndarray):
         self.high = +np.abs(noise)
