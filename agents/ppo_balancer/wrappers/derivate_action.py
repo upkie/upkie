0289591da855fcbce6cb8ad75297cd0eb67a8e90
Stéphane Caron <stephane.caron@normalesup.org>
2023-10-30 11:39:54 +0100

    Add DerivateAction environment wrapper

---
+++ agents/ppo_balancer/wrappers/derivate_action.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+# SPDX-License-Identifier: Apache-2.0
+
+import gymnasium
+import numpy as np
+from gymnasium import spaces
+from numpy.typing import NDArray
+
+
+class DerivateAction(gymnasium.Wrapper):
+    def __init__(
+        self,
+        env,
+        min_derivative: NDArray[float],
+        max_derivative: NDArray[float],
+    ):
+        """!
+        Act on the derivative of the action.
+
+        @param env Environment to wrap.
+        @param min_derivative Lower bound on the derivative of the original
+            action.
+        @param max_derivative Upper bound on the derivative of the original
+            action.
+
+        @note We assume the original action lives in a vector space.
+        """
+        super().__init__(env)
+        self.action_space = spaces.Box(
+            np.float32(min_derivative),
+            np.float32(max_derivative),
+            shape=env.action_space.shape,
+            dtype=np.float32,
+        )
+        self._integral = np.zeros(env.action_space.shape)
+
+    def reset(self, **kwargs):
+        self._integral = np.zeros(self.action_space.shape)
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        self._integral = np.clip(
+            self._integral + action * self.env.dt,
+            self.env.action_space.low,
+            self.env.action_space.high,
+        )
+        return self.env.step(self._integral)
