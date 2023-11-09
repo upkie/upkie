ea912f55955c1e295ca270aee6c3607402173cdb
Stéphane Caron <stephane.caron@normalesup.org>
2023-11-09 17:31:12 +0100

    [minor] Update run_trained_policy script

--- run_trained_policy.sh
+++ run_trained_policy.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-cp $(realpath $1) ./agents/ppo_balancer/policy/params.zip
-./tools/bazel run //agents/ppo_balancer:run $(realpath $1) -- --training
+POLICY_DIR=$(realpath $1)
+
+if [ ! -d "${POLICY_DIR}" ]; then
+    echo "${POLICY_DIR} is not a directory (or does not exist)."
+    exit 1
+fi
+
+CONFIG=$(realpath $1)/operative_config.gin
+PARAMS=$(realpath $1)/final.zip
+
+if [ ! -f "${CONFIG}" ]; then
+    echo "Configuration file ${CONFIG} does not exist."
+    exit 1
+fi
+
+if [ ! -f "${PARAMS}" ]; then
+    echo "Policy parameters ${PARAMS} do not exist."
+    exit 1
+fi
+
+cp -f ${CONFIG} ./agents/ppo_balancer/policy/operative_config.gin
+cp -f ${PARAMS} ./agents/ppo_balancer/policy/params.zip
+
+./tools/bazel run //agents/ppo_balancer:run ${PARAMS} -- --training
