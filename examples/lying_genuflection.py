3338c67a3a30eaf277e5114b3bb6f8bba95c6d06
Stéphane Caron <stephane.caron@normalesup.org>
2023-11-15 14:17:19 +0100

    Refactor lying genuflection example

--- examples/lying_genuflection.py
+++ examples/lying_genuflection.py
@@ -7,12 +7,16 @@
 
 import gymnasium as gym
 import numpy as np
-
 import upkie.envs
 
-nb_genuflections = 10
-genuflection_steps = 200
-amplitude = 1.0  # in radians
+NB_GENUFLECTIONS = 10
+GENUFLECTION_STEPS = 200
+AMPLITUDE = 1.0  # in radians
+
+LEFT_HIP = 0  # index of left hip position in vectorized action
+LEFT_KNEE = 1  # same for left knee, etc.
+RIGHT_HIP = 3
+RIGHT_KNEE = 4
 
 spine_config = {
     "bullet": {
@@ -26,19 +30,13 @@ spine_config = {
 if __name__ == "__main__":
     upkie.envs.register()
     with gym.make(
-        "UpkieServos-v2", spine_config=spine_config, frequency=200.0
+        "UpkieServos-v2", spine_config=spine_config, frequency=200.0,
     ) as env:
-        env.reset()  # connects to the spine
         action = np.zeros(env.action_space.shape)
-        for step in range(nb_genuflections * genuflection_steps):
-            observation, _, _, _, _ = env.step(action)
-            x = float(step % genuflection_steps) / genuflection_steps
+        observation, _ = env.reset()  # connects to the spine
+        for step in range(NB_GENUFLECTIONS * GENUFLECTION_STEPS):
+            x = float(step % GENUFLECTION_STEPS) / GENUFLECTION_STEPS
             y = 4.0 * x * (1.0 - x)  # in [0, 1]
-            A = amplitude  # in radians
-
-            left_hip = 0  # index of left hip position in vectorized action
-            left_knee = 1  # same for left knee, etc.
-            right_hip = 3
-            right_knee = 4
-            q_0134 = A * y * np.array([1.0, -2.0, -1.0, 2.0])
-            action[[left_hip, left_knee, right_hip, right_knee]] = q_0134
+            q_0134 = AMPLITUDE * y * np.array([1.0, -2.0, -1.0, 2.0])
+            action[[LEFT_HIP, LEFT_KNEE, RIGHT_HIP, RIGHT_KNEE]] = q_0134
+            observation, _, _, _, _ = env.step(action)
