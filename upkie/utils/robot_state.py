ca38cde0ffb1794b420c3a542d640adacee56d37
Stéphane Caron <stephane.caron@normalesup.org>
2024-01-12 11:47:18 +0100

    Add joint configuration and velocity to robot state

--- upkie/utils/robot_state.py
+++ upkie/utils/robot_state.py
@@ -36,11 +36,31 @@ class RobotState:
         angular_velocity_base_in_base: Optional[NDArray[float]] = None,
         joint_configuration: Optional[NDArray[float]] = None,
         joint_velocity: Optional[NDArray[float]] = None,
-        linear_velocity_base_to_world_in_world: Optional[ NDArray[float] ] = None,
+        linear_velocity_base_to_world_in_world: Optional[
+            NDArray[float]
+        ] = None,
         orientation_base_in_world: Optional[ScipyRotation] = None,
         position_base_in_world: Optional[NDArray[float]] = None,
         randomization: Optional[RobotStateRandomization] = None,
     ):
+        self.angular_velocity_base_in_base = (
+            angular_velocity_base_in_base
+            if angular_velocity_base_in_base is not None
+            else np.zeros(3)
+        )
+        self.joint_configuration = (
+            joint_configuration
+            if joint_configuration is not None
+            else np.zeros(6)
+        )
+        self.joint_velocity = (
+            joint_velocity if joint_velocity is not None else np.zeros(6)
+        )
+        self.linear_velocity_base_to_world_in_world = (
+            linear_velocity_base_to_world_in_world
+            if linear_velocity_base_to_world_in_world is not None
+            else np.zeros(3)
+        )
         self.orientation_base_in_world = (
             orientation_base_in_world
             if orientation_base_in_world is not None
@@ -51,40 +71,10 @@ class RobotState:
             if position_base_in_world is not None
             else np.zeros(3)
         )
-        self.linear_velocity_base_to_world_in_world = (
-            linear_velocity_base_to_world_in_world
-            if linear_velocity_base_to_world_in_world is not None
-            else np.zeros(3)
-        )
-        self.angular_velocity_base_in_base = (
-            angular_velocity_base_in_base
-            if angular_velocity_base_in_base is not None
-            else np.zeros(3)
-        )
         self.randomization = (
             randomization
             if randomization is not None
             else RobotStateRandomization()
-        )
-
-    def sample_orientation(self, np_random) -> ScipyRotation:
-        rotation_rand_to_base = self.randomization.sample_orientation(
-            np_random
-        )
-        rotation_base_to_world = self.orientation_base_in_world
-        return rotation_base_to_world * rotation_rand_to_base
-
-    def sample_position(self, np_random) -> NDArray[float]:
-        position_rand_in_world = self.randomization.sample_position(np_random)
-        return self.position_base_in_world + position_rand_in_world
-
-    def sample_linear_velocity(self, np_random) -> NDArray[float]:
-        linear_velocity_rand_to_world_in_world = (
-            self.randomization.sample_linear_velocity(np_random)
-        )
-        return (
-            self.linear_velocity_base_to_world_in_world
-            + linear_velocity_rand_to_world_in_world
         )
 
     def sample_angular_velocity(self, np_random) -> NDArray[float]:
@@ -98,3 +88,23 @@ class RobotState:
             angular_velocity_base_to_world_in_base
             + angular_velocity_rand_to_base_in_base
         )
+
+    def sample_linear_velocity(self, np_random) -> NDArray[float]:
+        linear_velocity_rand_to_world_in_world = (
+            self.randomization.sample_linear_velocity(np_random)
+        )
+        return (
+            self.linear_velocity_base_to_world_in_world
+            + linear_velocity_rand_to_world_in_world
+        )
+
+    def sample_orientation(self, np_random) -> ScipyRotation:
+        rotation_rand_to_base = self.randomization.sample_orientation(
+            np_random
+        )
+        rotation_base_to_world = self.orientation_base_in_world
+        return rotation_base_to_world * rotation_rand_to_base
+
+    def sample_position(self, np_random) -> NDArray[float]:
+        position_rand_in_world = self.randomization.sample_position(np_random)
+        return self.position_base_in_world + position_rand_in_world
