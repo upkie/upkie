b0c6c21c4e838df6530f222997c9941cf7cced42
pgraverdy <pgraverdy@gmail.com>
2024-01-25 22:14:03 +0100

    Fetch spine if available

--- start_simulation.sh
+++ start_simulation.sh
@@ -3,4 +3,60 @@
 SCRIPT=$(realpath "$0")
 SCRIPTDIR=$(dirname "${SCRIPT}")
 
-(cd ${SCRIPTDIR} && ${SCRIPTDIR}/tools/bazelisk run //spines:bullet_spine -- --show)
+
+VERSION=$(awk '/^PROJECT_NUMBER/{print $3}' docs/Doxyfile)
+
+SYSTEM=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ "$SYSTEM" == Darwin ]]; then
+    echo "MacOS system"
+    if [[ "$ARCH" == x86_64* ]]; then
+        echo "X64 Architecture"
+        SPINE_ARCHIVE="$URL_ARCHIVE"/v"$VERSION"/darwin_x86_bullet_spine.tar.gz
+    elif [[ "$ARCH" == i*86 ]]; then
+        echo "X32 Architecture"
+        SPINE_ARCHIVE="$URL_ARCHIVE"/v"$VERSION"/darwin_x86_bullet_spine.tar.gz
+    elif  [[ "$ARCH" == arm* ]]; then
+        echo "ARM Architecture"
+        SPINE_ARCHIVE="$URL_ARCHIVE"/v"$VERSION"/darwin_arm64_bullet_spine.tar.gz
+    else
+        echo "Unsupported architecture"
+    fi
+elif  [[ "$ARCH" == Linux ]]; then
+    echo "Linux system"
+    if [[ "$ARCH" == x86_64* ]]; then
+        echo "X64 Architecture"
+        SPINE_ARCHIVE="$URL_ARCHIVE"/v"$VERSION"/linux_amd64_bullet_spine.tar.gz
+    elif [[ "$ARCH" == i*86 ]]; then
+        echo "X32 Architecture"
+        SPINE_ARCHIVE="$URL_ARCHIVE"/v"$VERSION"/linux_i386_bullet_spine.tar.gz
+    else
+        echo "Unsupported architecture"
+    fi
+else
+    echo "Unsupported System"
+fi
+
+echo SPINE_ARCHIVE $SPINE_ARCHIVE
+
+
+if [[ -z "$SPINE_ARCHIVE" ]]; then
+    echo "Build spine and use";
+    (cd ${SCRIPTDIR} && ${SCRIPTDIR}/tools/bazelisk run //spines:bullet_spine -- --show)
+else
+    # check that the full operation works - use pipefail as it works for bash/zsh
+    (set -o pipefail;  curl -s -L $SPINE_ARCHIVE | tar xvz - -C .); RETCODE=$?
+
+    echo RETCODE $RETCODE
+
+    if [[ $RETCODE -eq 0 ]]; then
+        echo "Use downloaded spine";
+        exit;
+    else
+        echo "Unable to download, Build spine and use";
+        (cd ${SCRIPTDIR} && ${SCRIPTDIR}/tools/bazelisk run //spines:bullet_spine -- --show)
+    fi
+fi
+
+
