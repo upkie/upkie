6654a563be7e100ffce9ec8a1527aa9b4bba6de4
Stéphane Caron <stephane.caron@normalesup.org>
2024-02-14 17:20:16 +0100

    Switch from posix-ipc to Python standard library for shared memory

--- upkie/envs/tests/upkie_servos_test.py
+++ upkie/envs/tests/upkie_servos_test.py
@@ -9,7 +9,8 @@
 import unittest
 
 import numpy as np
-import posix_ipc
+from multiprocessing.shared_memory import SharedMemory
+
 
 from upkie.envs import UpkieServos
 from upkie.envs.tests.mock_spine import MockSpine
@@ -19,15 +20,13 @@ from upkie.utils.exceptions import ActionError
 class TestUpkieServos(unittest.TestCase):
     def setUp(self):
         shm_name = "/vroum"
-        shared_memory = posix_ipc.SharedMemory(
-            shm_name, posix_ipc.O_RDWR | posix_ipc.O_CREAT, size=42
-        )
+        shared_memory = SharedMemory(shm_name, size=42, create=True)
         self.env = UpkieServos(
             fall_pitch=1.0,
             frequency=100.0,
             shm_name=shm_name,
         )
-        shared_memory.close_fd()
+        shared_memory.close()
         self.env._spine = MockSpine()
 
     def test_reset(self):
