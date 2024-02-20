508835556ec6785050282c7f1ee6c546b74580c1
Stéphane Caron <stephane.caron@normalesup.org>
2024-02-20 15:41:09 +0100

    [minor] Refactor example

--- examples/wheeled_balancing_torque_control.py
+++ examples/wheeled_balancing_torque_control.py
@@ -7,7 +7,6 @@
 """Balancing using proportional control from base pitch to wheel torques."""
 
 import gymnasium as gym
-
 import upkie.envs
 from upkie.observers.base_pitch import compute_base_pitch_from_imu
 
@@ -15,26 +14,31 @@ upkie.envs.register()
 
 GAIN = 10.0  # base pitch to wheel torque, in [N] * [m] / [rad]
 
+
+def run(env: upkie.envs.UpkieServos):
+    action = env.get_neutral_action()
+
+    # Position commands to keep the legs extended
+    action["left_hip"]["position"] = 0.0
+    action["left_knee"]["position"] = 0.0
+    action["right_hip"]["position"] = 0.0
+    action["right_knee"]["position"] = 0.0
+
+    # Disable velocity feedback in the wheels
+    # (we don't set kp_scale as the neutral action has no position command)
+    action["left_wheel"]["kd_scale"] = 0.0
+    action["right_wheel"]["kd_scale"] = 0.0
+
+    obs, _ = env.reset()  # connects to the spine
+    for step in range(1_000_000):
+        pitch = compute_base_pitch_from_imu(obs["imu"]["orientation"])
+        action["left_wheel"]["feedforward_torque"] = +GAIN * pitch
+        action["right_wheel"]["feedforward_torque"] = -GAIN * pitch
+        obs, _, terminated, truncated, _ = env.step(action)
+        if terminated or truncated:
+            obs, _ = env.reset()
+
+
 if __name__ == "__main__":
     with gym.make("UpkieServos-v3", frequency=200.0) as env:
-        action = env.get_neutral_action()
-
-        # Position commands to keep the legs extended
-        action["left_hip"]["position"] = 0.0
-        action["left_knee"]["position"] = 0.0
-        action["right_hip"]["position"] = 0.0
-        action["right_knee"]["position"] = 0.0
-
-        # Disable velocity feedback in the wheels
-        # (we don't set kp_scale as the neutral action has no position command)
-        action["left_wheel"]["kd_scale"] = 0.0
-        action["right_wheel"]["kd_scale"] = 0.0
-
-        obs, _ = env.reset()  # connects to the spine
-        for step in range(1_000_000):
-            pitch = compute_base_pitch_from_imu(obs["imu"]["orientation"])
-            action["left_wheel"]["feedforward_torque"] = +GAIN * pitch
-            action["right_wheel"]["feedforward_torque"] = -GAIN * pitch
-            obs, _, terminated, truncated, _ = env.step(action)
-            if terminated or truncated:
-                obs, _ = env.reset()
+        run(env)
