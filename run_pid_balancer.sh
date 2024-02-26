dff2dfb6b6bc148b45d6a3c4ee9a69d295f6ab5a
Stéphane Caron <stephane.caron@normalesup.org>
2024-02-26 14:34:43 +0100

    Check if simulation is running in run script

--- run_pid_balancer.sh
+++ run_pid_balancer.sh
@@ -3,11 +3,13 @@
 SCRIPT=$(realpath "$0")
 SCRIPTDIR=$(dirname "${SCRIPT}")
 
-read -p "Did you start the simulation? (y/n) " yn
-
-case $yn in
-    [Yy]* ) echo "";;
-    * ) echo "You will first need to run ./start_simulation.sh"; exit;;
-esac
+if ! pgrep -f "bullet_spine" > /dev/null
+then
+    read -p "Simulation not detected, did you start it? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo "";;
+        * ) echo "You will first need to run ./start_simulation.sh"; exit;;
+    esac
+fi
 
 ${SCRIPTDIR}/tools/bazelisk run //pid_balancer -- -c bullet
