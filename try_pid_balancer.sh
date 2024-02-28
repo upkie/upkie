fca6d9af7d7e19205c2f896deeb008e37d00ee5a
Stéphane Caron <stephane.caron@inria.fr>
2024-02-28 11:35:41 +0100

    Run bullet target rather than bare agent

--- try_pid_balancer.sh
+++ try_pid_balancer.sh
@@ -3,13 +3,4 @@
 SCRIPT=$(realpath "$0")
 SCRIPTDIR=$(dirname "${SCRIPT}")
 
-if ! pgrep -f "bullet_spine" > /dev/null
-then
-    read -p "Simulation not detected, did you start it? (y/n) " yn
-    case $yn in
-        [Yy]* ) echo "";;
-        * ) echo "You will first need to run ./start_simulation.sh"; exit;;
-    esac
-fi
-
-${SCRIPTDIR}/tools/bazelisk run //pid_balancer -- -c bullet
+${SCRIPTDIR}/tools/bazelisk run //pid_balancer:bullet
