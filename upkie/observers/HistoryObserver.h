4fe7b4c2019719ba2b39daf0f8f77ab8103e6630
Stéphane Caron <stephane.caron@inria.fr>
2024-05-24 17:51:49 +0200

    Implement HistoryObserver and test on double

--- upkie/observers/HistoryObserver.h
+++ upkie/observers/HistoryObserver.h
@@ -10,11 +10,13 @@
 #include <unordered_map>
 #include <vector>
 
+#include "vulp/exceptions/TypeError.h"
 #include "vulp/observation/Observer.h"
 
 namespace vulp::observation {
 
 using palimpsest::Dictionary;
+using vulp::exceptions::TypeError;
 using vulp::observation::Observer;
 
 template <typename T>
@@ -25,47 +27,25 @@ class HistoryObserver : public Observer {
    * \param[in] default_value Value to initialize history vectors.
    * \param[in] prefix Dictionary key where outputs will be written.
    */
-  explicit HistoryObserver(const T& default_value,
-                           const std::string& prefix = "")
-      : default_value_(default_value),
-        prefix_((prefix.size() > 1) ? prefix : "history_observer") {}
+  HistoryObserver(const std::vector<std::string>& keys, size_t size,
+                  const T& default_value)
+      : keys_(keys), values_(size, default_value) {}
 
   //! Prefix of outputs in the observation dictionary.
-  inline std::string prefix() const noexcept final { return prefix_; }
+  inline std::string prefix() const noexcept final { return "history"; }
 
   /*! Reset observer.
    *
    * \param[in] config Overall configuration dictionary.
    */
-  void reset(const Dictionary& config) override {
-    if (!config.has(prefix_)) {
-      spdlog::debug("No \"{}\" runtime configuration", prefix_);
-      return;
-    }
-
-    spdlog::info("Applying \"{}\" runtime configuration", prefix_);
-    values_.clear();
-    reset_values(config(prefix_), values_);
-  }
-
-  void reset_values(const Dictionary& source, Dictionary& dest) {
-    for (const std::string& key : source.keys()) {
-      const Dictionary& child = source(key);
-      if (child.is_value()) {
-        const unsigned size = child.as<unsigned>();
-        dest.insert<std::vector<T>>(key, size, default_value_);
-      } else {
-        reset_values(child, dest(key));
-      }
-    }
-  }
+  void reset(const Dictionary& config) final {}
 
   /*! Read inputs from other observations.
    *
    * \param[in] observation Dictionary to read other observations from.
    */
   void read(const Dictionary& observation) final {
-    // ...
+    return read_value(observation, 0);
   }
 
   /*! Write outputs, called if reading was successful.
@@ -73,16 +53,52 @@ class HistoryObserver : public Observer {
    * \param[out] observation Dictionary to write observations to.
    */
   void write(Dictionary& observation) final {
-    // ...
+    auto& output = observation(prefix());
+    write_values(output, 0);
   }
 
  private:
-  //! Prefix of outputs in the observation dictionary.
-  std::string prefix_;
+  std::string concatenate_keys() {
+    std::string output;
+    for (const auto& key : keys_) {
+      output += "/" + key;
+    }
+    return output;
+  }
 
-  T default_value_;
+  void read_value(const Dictionary& dict, unsigned key_index) {
+    if (key_index != keys_.size()) {
+      const std::string& key = keys_.at(key_index);
+      return read_value(dict(key), key_index + 1);
+    }
+    if (!dict.is_value()) {
+      throw TypeError(
+          __FILE__, __LINE__,
+          "Observation at " + concatenate_keys() + " is not a value");
+    }
+    const T& new_value = dict.as<T>();
+    values_.pop_back();
+    values_.insert(values_.begin(), new_value);
+  }
 
-  Dictionary values_;
+  void write_values(Dictionary& dict, unsigned key_index) {
+    if (key_index != keys_.size()) {
+      const std::string& key = keys_.at(key_index);
+      return write_values(dict(key), key_index + 1);
+    }
+    if (dict.is_empty()) {
+      dict = values_;
+    } else {
+      dict.as<std::vector<T>>() = values_;
+    }
+  }
+
+ private:
+  //! Keys locating values to read from in input observations.
+  std::vector<std::string> keys_;
+
+  //! History of recent values written to output observations.
+  std::vector<T> values_;
 };
 
 }  // namespace vulp::observation
