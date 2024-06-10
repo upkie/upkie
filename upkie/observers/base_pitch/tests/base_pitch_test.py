67ea982cb55a9bccfd339171b521c7244d815842
Stéphane Caron <stephane.caron@inria.fr>
2024-06-10 08:14:13 +0200

    WIP: Move another unit test from Python to C++

--- upkie/observers/base_pitch/tests/base_pitch_test.py
+++ upkie/observers/base_pitch/tests/base_pitch_test.py
@@ -19,20 +19,6 @@ from upkie.observers.base_pitch import (
 
 
 class TestBasePitch(unittest.TestCase):
-    def test_close_to_zero(self, theta=1e-3):
-        """
-        Check that a pure rotation around the y-axis yields the correct pitch.
-        """
-        orientation_imu_in_world = np.array(
-            [
-                [np.cos(theta), 0.0, np.sin(theta)],
-                [0.0, 1.0, 0.0],
-                [-np.sin(theta), 0.0, np.cos(theta)],
-            ]
-        )
-        imu_pitch = compute_pitch_frame_in_parent(orientation_imu_in_world)
-        self.assertTrue(np.isclose(imu_pitch, theta))
-
     def test_orientation_not_neatly_normalized(self, theta=1e-3):
         """
         As an extra, the function can handle a partially-normalized input. We
