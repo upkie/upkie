a2fb60df0090362f5c6d206d0f57658254767178
Stéphane Caron <stephane.caron@inria.fr>
2024-06-10 08:11:57 +0200

    WIP: Move unit test from Python to C++

--- upkie/observers/tests/BaseOrientationTest.cpp
+++ upkie/observers/tests/BaseOrientationTest.cpp
@@ -32,7 +32,17 @@ class BaseOrientationTest : public ::testing::Test {
   Eigen::Matrix3d rotation_ars_to_world_;
 };
 
-TEST_F(BaseOrientationTest, Lateral) {
+// Check that a pure rotation around the z-axis yields no pitch.
+TEST_F(BaseOrientationTest, ZeroPitch) {
+  const double phi = 0.42;
+  Eigen::Matrix3d orientation_imu_in_world;
+  orientation_imu_in_world << cos(phi), -sin(phi), 0.0, sin(phi), cos(phi), 0.0,
+      0.0, 0.0, 1.0;
+  double imu_pitch = compute_pitch_frame_in_parent(orientation_imu_in_world);
+  ASSERT_DOUBLE_EQ(imu_pitch, 0.0);
+}
+
+TEST_F(BaseOrientationTest, BasePitchFromIMU) {
   Eigen::Quaterniond quat_imu_in_ars = {
       0.008472769239730098,   // w
       -0.9953038144146671,    // x
