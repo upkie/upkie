7129e4bd6612bbfd5553deecbb7be0001e929a74
Stéphane Caron <stephane.caron@inria.fr>
2024-06-11 09:20:25 +0200

    docs: Document robot state functions

--- upkie/utils/robot_state.py
+++ upkie/utils/robot_state.py
@@ -63,6 +63,23 @@ class RobotState:
         position_base_in_world: Optional[NDArray[float]] = None,
         randomization: Optional[RobotStateRandomization] = None,
     ):
+        r"""!
+        Initialize a new state.
+
+        \param[in] angular_velocity_base_in_base Body angular velocity of the
+            floating-base link.
+        \param[in] joint_configuration Joint configuration vector (not
+            including the floating-base joint).
+        \param[in] joint_velocity Joint velocity vector (not including the
+            floating-base joint).
+        \param[in] linear_velocity_base_to_world_in_world Linear velocity of
+            the floating-base link in the world frame.
+        \param[in] orientation_base_in_world Rotation from the floating-base
+            frame to the world frame.
+        \param[in] position_base_in_world Position of the floating-base frame
+            in the world frame.
+        \param[in] randomization Optional state randomization distribution.
+        """
         self.angular_velocity_base_in_base = (
             angular_velocity_base_in_base
             if angular_velocity_base_in_base is not None
@@ -97,7 +114,15 @@ class RobotState:
             else RobotStateRandomization()
         )
 
-    def sample_angular_velocity(self, np_random) -> NDArray[float]:
+    def sample_angular_velocity(
+        self, np_random: np.random.Generator
+    ) -> NDArray[float]:
+        r"""!
+        Sample an angular velocity around the one in this state.
+
+        \param[in] np_random NumPy random number generator.
+        \return Sampled angular velocity.
+        """
         angular_velocity_rand_to_base_in_base = (
             self.randomization.sample_angular_velocity(np_random)
         )
@@ -109,7 +134,15 @@ class RobotState:
             + angular_velocity_rand_to_base_in_base
         )
 
-    def sample_linear_velocity(self, np_random) -> NDArray[float]:
+    def sample_linear_velocity(
+        self, np_random: np.random.Generator
+    ) -> NDArray[float]:
+        r"""!
+        Sample a linear velocity around the one in this state.
+
+        \param[in] np_random NumPy random number generator.
+        \return Sampled linear velocity.
+        """
         linear_velocity_rand_to_world_in_world = (
             self.randomization.sample_linear_velocity(np_random)
         )
@@ -118,13 +151,29 @@ class RobotState:
             + linear_velocity_rand_to_world_in_world
         )
 
-    def sample_orientation(self, np_random) -> ScipyRotation:
+    def sample_orientation(
+        self, np_random: np.random.Generator
+    ) -> ScipyRotation:
+        r"""!
+        Sample an orientation around the one in this state.
+
+        \param[in] np_random NumPy random number generator.
+        \return Sampled orientation.
+        """
         rotation_rand_to_base = self.randomization.sample_orientation(
             np_random
         )
         rotation_base_to_world = self.orientation_base_in_world
         return rotation_base_to_world * rotation_rand_to_base
 
-    def sample_position(self, np_random) -> NDArray[float]:
+    def sample_position(
+        self, np_random: np.random.Generator
+    ) -> NDArray[float]:
+        r"""!
+        Sample a position around the one in this state.
+
+        \param[in] np_random NumPy random number generator.
+        \return Randomized position.
+        """
         position_rand_in_world = self.randomization.sample_position(np_random)
         return self.position_base_in_world + position_rand_in_world
