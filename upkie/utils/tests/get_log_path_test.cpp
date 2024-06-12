b2442f7cb99f1985a6e928fe622b84bdb637ca36
Stéphane Caron <stephane.caron@inria.fr>
2024-06-12 11:13:07 +0200

    Fix get_log_path unit tests

--- upkie/utils/tests/get_log_path_test.cpp
+++ upkie/utils/tests/get_log_path_test.cpp
@@ -10,12 +10,12 @@
 namespace upkie::utils {
 
 TEST(GetLogPath, StartsWithLogdir) {
-  const std::string path = get_log_path("foo_spine", "bar_dir");
-  ASSERT_EQ(path.rfind("bar_dir", 0), 0);
+  const std::string path = get_log_path("log_dir", "spine_name");
+  ASSERT_EQ(path.rfind("log_dir", 0), 0);
 }
 
 TEST(GetLogPath, EndsWithMpack) {
-  const std::string path = get_log_path("foo_spine", "bar_dir");
+  const std::string path = get_log_path("log_dir", "spine_name");
   const std::string suffix = ".mpack";
   ASSERT_EQ(path.rfind(suffix), path.size() - suffix.size());
 }
