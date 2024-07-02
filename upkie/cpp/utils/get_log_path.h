020eea53a1f28e7b245fc7d61687c6f1dc7f0b1e
Stéphane Caron <stephane.caron@inria.fr>
2024-07-02 22:37:14 +0200

    Put hostname before spine name in log file names

--- upkie/cpp/utils/get_log_path.h
+++ upkie/cpp/utils/get_log_path.h
@@ -26,16 +26,14 @@ namespace upkie {
  */
 inline const std::string get_log_path(const std::string& log_dir,
                                       const std::string& spine_name) {
-  const auto now = datetime_now_string();
-  const std::string prefix = log_dir + "/" + now + "_" + spine_name;
-
+  const std::string prefix = log_dir + "/" + datetime_now_string() + "_";
   char hostname[512];
   int return_code = gethostname(hostname, 512);
   if (return_code != 0) {
     spdlog::warn("Could not get the robot's hostname, errno = {}", errno);
-    return prefix + ".mpack";
+    return prefix + spine_name + ".mpack";
   }
-  return prefix + "_" + hostname + ".mpack";
+  return prefix + hostname + "_" + spine_name + ".mpack";
 }
 
 }  // namespace upkie
