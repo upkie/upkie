92a4a4390b99f9c9c7cdf1071f4eee3591611725
Stéphane Caron <stephane.caron@inria.fr>
2024-07-05 19:24:01 +0200

    WIP: Read joint names from URDF directly

--- upkie/model/__init__.py
+++ upkie/model/__init__.py
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Inria
 
-from .joints import upper_leg_joints
+from .model import Model
 
 __all__ = [
-    "upper_leg_joints",
+    "Model",
 ]
