2a6178f6bba33a3c5645523cebd635fd0a3886de
Stéphane Caron <stephane.caron@inria.fr>
2024-07-05 17:59:22 +0200

    WIP: Update __init__.py of upkie.model

--- upkie/model/__init__.py
+++ upkie/model/__init__.py
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Inria
 
-from .model import Model
+from .joints import upper_leg_joints
 
 __all__ = [
-    "Model",
+    "upper_leg_joints",
 ]
