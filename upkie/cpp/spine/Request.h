21c4011789f41ba47afa127ba3d3c82968243e9b
Stéphane Caron <stephane.caron@inria.fr>
2024-07-29 08:51:00 +0200

    docs: Document values of C++ request enum

--- upkie/cpp/spine/Request.h
+++ upkie/cpp/spine/Request.h
@@ -10,12 +10,23 @@ namespace upkie::spine {
 
 //! Request flag used for shared-memory inter-process communication.
 enum class Request : uint32_t {
-  kNone = 0,  // no active request
+  //! Flag set when there is no active request.
+  kNone = 0,
+
+  //! Flag set to indicate an observation is requested.
   kObservation = 1,
+
+  //! Flag set to indicate an action has been supplied.
   kAction = 2,
+
+  //! Flag set to start the spine.
   kStart = 3,
+
+  //! Flag set to stop the spine.
   kStop = 4,
-  kError = 5  // last request was invalid
+
+  //! Flag set when the last request was invalid.
+  kError = 5
 };
 
 }  // namespace upkie::spine
