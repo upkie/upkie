400547cfce69652ada6e017bd133334cacbe3fa4
Stéphane Caron <stephane.caron@inria.fr>
2024-07-29 08:21:11 +0200

    Add provisioning script to complete image build

---
+++ tools/raspios/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Configure CPU isolation
+python /root/configure_cpu_isolation.py
+
+# Install Debian packages
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y python3-pip tmux vim
+
+# Install packages from PyPI
+pip install moteus-pi3hat upkie
+
+# Set binary permissions
+chmod 755 /usr/local/bin/hard_rezero
+chmod 755 /usr/local/bin/micromamba
+chmod 755 /usr/local/bin/pi3hat_spine
+chmod 755 /usr/local/bin/stop_servos
+chmod 755 /usr/local/bin/upkie_tool
+
+# Configure micromamba
+/usr/local/bin/micromamba config append channels conda-forge
+/usr/local/bin/micromamba config append channels nodefaults
+/usr/local/bin/micromamba config set channel_priority strict
