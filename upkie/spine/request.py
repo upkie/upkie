5c5afd956bc65d76306b84686c665fabb07b913f
Stéphane Caron <stephane.caron@inria.fr>
2024-07-29 11:35:42 +0200

    docs: Temporary fix to Doxygen 1.9 error
    Actually the Request class does not appear in the output docs...

--- upkie/spine/request.py
+++ upkie/spine/request.py
@@ -13,28 +13,28 @@ class Request(IntEnum):
 
     Once the spine has processed the request, it resets the flag to \ref kNone
     in shared memory.
+
+    @var kNone
+    Flag set when there is no active request.
+
+    @var kObservation
+    Flag set to indicate an observation is requested.
+
+    @var kAction
+    Flag set to indicate an action has been supplied.
+
+    @var kStart
+    Flag set to start the spine.
+
+    @var kStop
+    Flag set to stop the spine.
+
+    @var kError
+    Flag set when the last request was invalid.
     """
-
-    ## @var kNone
-    ## Flag set when there is no active request.
     kNone = 0
-
-    ## @var kObservation
-    ## Flag set to indicate an observation is requested.
     kObservation = 1
-
-    ## @var kAction
-    ## Flag set to indicate an action has been supplied.
     kAction = 2
-
-    ## @var kStart
-    ## Flag set to start the spine.
     kStart = 3
-
-    ## @var kStop
-    ## Flag set to stop the spine.
     kStop = 4
-
-    ## @var kError
-    ## Flag set when the last request was invalid.
     kError = 5
