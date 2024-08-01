ae941353d99ac8156a2b03213c9236437d78b3b1
Stéphane Caron <stephane.caron@inria.fr>
2024-08-01 19:34:09 +0200

    Add unit test for raw angular velocity function

--- upkie/cpp/actuation/pi3hat/tests/imu_test.cpp
+++ upkie/cpp/actuation/pi3hat/tests/imu_test.cpp
@@ -10,6 +10,15 @@
 
 namespace upkie::cpp::actuation::pi3hat {
 
+TEST(IMU, RawAngularVelocity) {
+  Eigen::Vector3d rate_dps = {180.0, -90.0, -0.0};
+  Eigen::Vector3d bias_dps = {90.0, 90.0, 90.0};
+  Eigen::Vector3d gyro = get_raw_angular_velocity(rate_dps, bias_dps);
+  ASSERT_NEAR(gyro.x(), M_PI / 2, 1e-12);
+  ASSERT_NEAR(gyro.y(), -M_PI, 1e-12);
+  ASSERT_NEAR(gyro.z(), -M_PI / 2, 1e-12);
+}
+
 TEST(IMU, RawLinearAcceleration1) {
   auto orientation_imu_in_ars = Eigen::Quaterniond::Identity();
   Eigen::Vector3d accel_mps2 = {0.1, 0.2, 0.0};
