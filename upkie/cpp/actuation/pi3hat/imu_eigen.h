2518f085411c5c287feab41145de58e389f93791
Stéphane Caron <stephane.caron@inria.fr>
2024-08-01 19:38:32 +0200

    docs: Add note on why we reverse filter outputs

--- upkie/cpp/actuation/pi3hat/imu_eigen.h
+++ upkie/cpp/actuation/pi3hat/imu_eigen.h
@@ -13,6 +13,10 @@ namespace upkie::cpp::actuation::pi3hat {
  *
  * \param[in] attitude Attitude object from the pi3hat library.
  * \return Raw angular velocity of the IMU in [rad] / [s].
+ *
+ * \note We reverse filter outputs for now as (1) it avoids an extra call to
+ * ReadSpi, and more generally customizing the Pi3Hat::Impl from mjbots, and
+ * (2) the pi3hat filter's code has been stable for the last four years.
  */
 inline Eigen::Vector3d get_raw_angular_velocity(
     const Eigen::Vector3d& rate_dps, const Eigen::Vector3d& bias_dps) noexcept {
@@ -26,6 +30,10 @@ inline Eigen::Vector3d get_raw_angular_velocity(
  * \param[in] orientation_imu_in_ars Rotation from the IMU to the ARS.
  * \param[in] accel_mps2 UKF output linear acceleration, in [m] / [s]².
  * \return Raw linear acceleration in [m] / [s]².
+ *
+ * \note We reverse filter outputs for now as (1) it avoids an extra call to
+ * ReadSpi, and more generally customizing the Pi3Hat::Impl from mjbots, and
+ * (2) the pi3hat filter's code has been stable for the last four years.
  */
 inline Eigen::Vector3d get_raw_linear_acceleration(
     const Eigen::Quaterniond& orientation_imu_in_ars,
@@ -36,8 +44,8 @@ inline Eigen::Vector3d get_raw_linear_acceleration(
       orientation_imu_in_ars.conjugate() * downward_in_world;
 
   // https://github.com/mjbots/pi3hat/blob/4a3158c831da125fa9c96d64378515c1fdb2083f/fw/attitude_reference.h#L147
-  Eigen::Vector3d current_accel_mps2_ = accel_mps2 + 9.81 * downward_in_imu;
-  return current_accel_mps2_;
+  Eigen::Vector3d current_accel_mps2 = accel_mps2 + 9.81 * downward_in_imu;
+  return current_accel_mps2;
 }
 
 }  // namespace upkie::cpp::actuation::pi3hat
