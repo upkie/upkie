b0e8d75e97240102b15402e6bed92d2d443e159b
Stéphane Caron <stephane.caron@inria.fr>
2024-08-02 14:47:56 +0200

    Double-check existing unit test

--- upkie/cpp/actuation/pi3hat/tests/imu_test.cpp
+++ upkie/cpp/actuation/pi3hat/tests/imu_test.cpp
@@ -19,17 +19,22 @@ TEST(IMU, RawAngularVelocity) {
   ASSERT_NEAR(gyro.z(), -M_PI / 2, 1e-12);
 }
 
-TEST(IMU, RawLinearAcceleration1) {
+TEST(IMU, RawLinearAccelerationAtRest) {
+  // At rest, with the Upkie in its zero configuration, the IMU frame is
+  // oriented with x to the rear of the robot, y to its left and z downward.
   auto orientation_imu_in_ars = Eigen::Quaterniond::Identity();
-  Eigen::Vector3d accel_mps2 = {0.1, 0.2, 0.0};
+  auto accel_mps2 = Eigen::Vector3d::Zero();
   Eigen::Vector3d imu_accel_mps2 =
       get_raw_linear_acceleration(orientation_imu_in_ars, accel_mps2);
-  ASSERT_NEAR(imu_accel_mps2.x(), 0.1, 1e-12);
-  ASSERT_NEAR(imu_accel_mps2.y(), 0.2, 1e-12);
+
+  // Raw acceleration should be accel_imu - gravity_imu = [0 0 -g]
+  // Note: gravity_imu = [0 0 +g] as the z-axis of the IMU frame is downward
+  ASSERT_NEAR(imu_accel_mps2.x(), 0.0, 1e-12);
+  ASSERT_NEAR(imu_accel_mps2.y(), 0.0, 1e-12);
   ASSERT_NEAR(imu_accel_mps2.z(), -9.81, 1e-12);
 }
 
-TEST(IMU, RawLinearAcceleration2) {
+TEST(IMU, RawLinearAccelerationQuarterXTurn) {
   Eigen::Quaterniond orientation_imu_in_ars(
       Eigen::AngleAxisd(-0.5 * M_PI, Eigen::Vector3d::UnitX()));
   Eigen::Vector3d accel_mps2 = {0.1, 0.0, 0.2};
