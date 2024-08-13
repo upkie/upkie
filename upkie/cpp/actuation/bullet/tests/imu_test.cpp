485a9855f03a37371e75b3c164df935980812860
Stéphane Caron <stephane.caron@inria.fr>
2024-08-13 17:01:57 +0200

    bullet: Separate unit test fixture for IMU

---
+++ upkie/cpp/actuation/bullet/tests/imu_test.cpp
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Stéphane Caron
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "RobotSimulator/b3RobotSimulatorClientAPI.h"
+#include "gtest/gtest.h"
+#include "tools/cpp/runfiles/runfiles.h"
+#include "upkie/cpp/actuation/BulletImuData.h"
+#include "upkie/cpp/actuation/bullet/utils.h"
+
+using bazel::tools::cpp::runfiles::Runfiles;
+
+namespace upkie::cpp::actuation::bullet {
+
+class BulletTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    std::string error;
+    std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest(&error));
+    ASSERT_NE(runfiles, nullptr);
+
+    std::string urdf_path =
+        runfiles->Rlocation("upkie_description/urdf/upkie.urdf");
+
+    bullet_ = std::make_unique<b3RobotSimulatorClientAPI>();
+    bool is_connected = bullet_->connect(eCONNECT_DIRECT);
+    ASSERT_TRUE(is_connected);
+    robot_ = bullet_->loadURDF(urdf_path);
+  }
+
+ protected:
+  //! Bullet client
+  std::unique_ptr<b3RobotSimulatorClientAPI> bullet_;
+
+  //! Robot identifier
+  int robot_;
+};
+
+TEST_F(BulletTest, IMULinearAccelerationSeesGravityInFreeFall) {
+  const int imu_link_index = find_link_index(*bullet_, robot_, "imu");
+  const double dt = 1. / 240.;  // [s]
+
+  // Remove simulator damping to get actual free fall physics
+  b3RobotSimulatorChangeDynamicsArgs change_dynamics_args;
+  change_dynamics_args.m_linearDamping = 0.0;
+  change_dynamics_args.m_angularDamping = 0.0;
+  bullet_->changeDynamics(robot_, -1, change_dynamics_args);
+  bullet_->setGravity(btVector3(0, 0, -4.2));
+  bullet_->setTimeStep(dt);
+
+  // Rotate the base so that the IMU frame has its x-axis downward
+  auto position_base_in_world = btVector3(0., 0., 0.);
+  auto orientation_base_in_world =
+      bullet_->getQuaternionFromEuler(btVector3(0., -M_PI / 2, 0.));
+  bullet_->resetBasePositionAndOrientation(robot_, position_base_in_world,
+                                           orientation_base_in_world);
+
+  BulletImuData imu_data;
+  read_imu_data(imu_data, *bullet_, robot_, imu_link_index, dt);
+  bullet_->stepSimulation();
+  read_imu_data(imu_data, *bullet_, robot_, imu_link_index, dt);
+  ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.x(), 4.2, 1e-10);
+  ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.y(), 0.0, 1e-10);
+  ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.z(), 0.0, 1e-10);
+}
+
+}  // namespace upkie::cpp::actuation::bullet
