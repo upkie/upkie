e800879c608122b7ac191b4f634e73e1c785675c
Stéphane Caron <stephane.caron@inria.fr>
2024-08-13 17:11:47 +0200

    Add unit test for raw acceleration

--- upkie/cpp/actuation/bullet/tests/imu_test.cpp
+++ upkie/cpp/actuation/bullet/tests/imu_test.cpp
@@ -67,4 +67,39 @@ TEST_F(BulletIMUTest, IMULinearAccelerationSeesGravityInFreeFall) {
   ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.z(), 0.0, 1e-10);
 }
 
+TEST_F(BulletIMUTest, RawIMULinearAcceleration) {
+  const int imu_link_index = find_link_index(*bullet_, robot_, "imu");
+  const double dt = 1. / 220.;  // [s]
+
+  // Remove simulator damping to get actual free fall physics
+  b3RobotSimulatorChangeDynamicsArgs change_dynamics_args;
+  change_dynamics_args.m_linearDamping = 0.0;
+  change_dynamics_args.m_angularDamping = 0.0;
+  bullet_->changeDynamics(robot_, -1, change_dynamics_args);
+  bullet_->setGravity(btVector3(0, 0, -kGravity));
+  bullet_->setTimeStep(dt);
+
+  // Rotate the base so that the IMU frame has its x-axis downward
+  auto position_base_in_world = btVector3(0., 0., 0.);
+  auto orientation_base_in_world =
+      bullet_->getQuaternionFromEuler(btVector3(0., -M_PI / 2, 0.));
+  bullet_->resetBasePositionAndOrientation(robot_, position_base_in_world,
+                                           orientation_base_in_world);
+
+  BulletImuData imu_data;
+  read_imu_data(imu_data, *bullet_, robot_, imu_link_index, dt);
+  bullet_->stepSimulation();
+  read_imu_data(imu_data, *bullet_, robot_, imu_link_index, dt);
+
+  // Raw measurement is the proper acceleration
+  ASSERT_NEAR(imu_data.raw_linear_acceleration.x(), 0.0, 1e-10);
+  ASSERT_NEAR(imu_data.raw_linear_acceleration.y(), 0.0, 1e-10);
+  ASSERT_NEAR(imu_data.raw_linear_acceleration.z(), 0.0, 1e-10);
+
+  // Filtered acceleration substracts gravity
+  ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.x(), kGravity, 1e-10);
+  ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.y(), 0.0, 1e-10);
+  ASSERT_NEAR(imu_data.linear_acceleration_imu_in_imu.z(), 0.0, 1e-10);
+}
+
 }  // namespace upkie::cpp::actuation::bullet
