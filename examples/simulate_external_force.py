b67182f9832461fc2f81ee7607b3a416d555214c
Stéphane Caron <stephane.caron@inria.fr>
2024-08-15 17:36:42 +0200

    Tune example parameters for a clearer demo

--- examples/simulate_external_force.py
+++ examples/simulate_external_force.py
@@ -16,23 +16,23 @@ upkie.envs.register()
 
 def get_vertical_force(
     step: int,
-    start: int = 400,
-    lift_steps: int = 100,
+    start: int = 200,
+    lift_steps: int = 200,
     delta: float = 0.1,
-    hold_steps: int = 200,
+    hold_steps: int = 400,
 ) -> float:
-    lift: float = 0.0  # no unit
+    lift: float = 0.0  # 0 = no force, 1 => apply -mg
     if step < start:
         lift = 0.0
     elif step - start < lift_steps // 2:
-        lift = 1 + delta
+        lift = 1.0 + delta
     elif step - start < lift_steps:
-        lift = 1 - delta
+        lift = 1.0 - delta
     elif step - start - lift_steps < hold_steps:
         lift = 1.0
     else:
         lift = 0.0
-    mass = 5.375  # [kg]
+    mass = 5.34  # approximative, in [kg]
     return lift * mass * 9.81  # in [N]
 
 
@@ -51,6 +51,8 @@ def run(env: upkie.envs.UpkieGroundVelocity):
     for step in range(1_000_000):
         action = gain.dot(observation).reshape((1,))
         torso_force_in_world[2] = get_vertical_force(step % 1000)
+        if torso_force_in_world[2] > 1.0:
+            action *= 0.0
         env.bullet_extra(bullet_action)  # call before env.step
         observation, _, terminated, truncated, _ = env.step(action)
         if terminated or truncated:
@@ -58,5 +60,5 @@ def run(env: upkie.envs.UpkieGroundVelocity):
 
 
 if __name__ == "__main__":
-    with gym.make("UpkieGroundVelocity-v3", frequency=200.0, frequency_checks=False) as env:
+    with gym.make("UpkieGroundVelocity-v3", frequency=200.0) as env:
         run(env)
