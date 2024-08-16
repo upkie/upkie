03382fc2779393e9cca4b39284ad6eb8ffb80ad4
Stéphane Caron <stephane.caron@inria.fr>
2024-08-16 11:22:25 +0200

    Add pure-bias IMU uncertainty unit test

--- upkie/cpp/actuation/tests/ImuUncertaintyTest.cpp
+++ upkie/cpp/actuation/tests/ImuUncertaintyTest.cpp
@@ -20,13 +20,24 @@ class ImuUncertaintyTest : public ::testing::Test {
   std::unique_ptr<std::mt19937> rng_;
 };
 
-TEST_F(ImuUncertaintyTest, NoNoise) {
+TEST_F(ImuUncertaintyTest, NoBiasNoNoise) {
   ImuUncertainty imu_uncertainty;
   Eigen::Vector3d linear_acceleration = Eigen::Vector3d::Zero();
   Eigen::Vector3d angular_velocity = Eigen::Vector3d::Zero();
   imu_uncertainty.apply(linear_acceleration, angular_velocity, *rng_);
   ASSERT_LT(linear_acceleration.norm(), 1e-10);
   ASSERT_LT(angular_velocity.norm(), 1e-10);
+}
+
+TEST_F(ImuUncertaintyTest, PureBias) {
+  ImuUncertainty imu_uncertainty;
+  imu_uncertainty.accelerometer_bias = 1.0;
+  imu_uncertainty.gyroscope_bias = 0.1;
+  Eigen::Vector3d linear_acceleration = Eigen::Vector3d::Zero();
+  Eigen::Vector3d angular_velocity = Eigen::Vector3d::Zero();
+  imu_uncertainty.apply(linear_acceleration, angular_velocity, *rng_);
+  ASSERT_NEAR(linear_acceleration.y(), 1.0, 1e-10);
+  ASSERT_NEAR(angular_velocity.z(), 0.1, 1e-10);
 }
 
 }  // namespace upkie::cpp::actuation
