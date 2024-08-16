98825a3e8d82b2b5c7f41e086ac6a9c08558e02d
Stéphane Caron <stephane.caron@inria.fr>
2024-08-16 11:45:26 +0200

    Annotate sensor noise example

--- examples/simulate_sensor_noise.py
+++ examples/simulate_sensor_noise.py
@@ -12,9 +12,10 @@ import numpy as np
 import upkie.envs
 
 # The following values are completely arbitrary :)
-JOINT_PROPS = {
-    "torque_control_noise": 0.1,  # white noise in [N m]
-    "torque_measurement_noise": 0.5,  # white noise in [N m]
+JOINT_NOISE = {
+    # Try increasing torque control noise: it affects balancing
+    "torque_control_noise": 0.5,  # white noise in [N m]
+    "torque_measurement_noise": 0.1,  # white noise in [N m]
 }
 
 if __name__ == "__main__":
@@ -25,22 +26,25 @@ if __name__ == "__main__":
         spine_config={
             "bullet": {
                 "imu_uncertainty": {
+                    "accelerometer_bias": [1e-3, 1e-3, 5e-3],
                     "accelerometer_noise": 0.2,
+                    # Try increasing noise or y-axis bias: it affects balancing
+                    "gyroscope_bias": [0.0, 2e-1, 0.0],
                     "gyroscope_noise": 0.05,
                 },
                 "joint_properties": {
-                    "left_hip": JOINT_PROPS,
-                    "left_knee": JOINT_PROPS,
-                    "left_wheel": JOINT_PROPS,
-                    "right_hip": JOINT_PROPS,
-                    "right_knee": JOINT_PROPS,
-                    "right_wheel": JOINT_PROPS,
+                    "left_hip": JOINT_NOISE,
+                    "left_knee": JOINT_NOISE,
+                    "left_wheel": JOINT_NOISE,
+                    "right_hip": JOINT_NOISE,
+                    "right_knee": JOINT_NOISE,
+                    "right_wheel": JOINT_NOISE,
                 },
             }
         },
     ) as env:
         observation, _ = env.reset()  # connects to the spine
-        gain = np.array([10.0, 1.0, 0.0, 0.1])
+        gain = np.array([10.0, 1.0, 0.1, 0.1])
         for step in range(1_000_000):
             # this is the same agent as in wheeled_balancing.py
             action = gain.dot(observation).reshape((1,))
