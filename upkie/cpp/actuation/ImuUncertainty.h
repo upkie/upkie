b9f856a499720d575cf9316a4c970f7193d6df90
Stéphane Caron <stephane.caron@inria.fr>
2024-08-16 10:24:29 +0200

    Add gyroscope uncertainties

--- upkie/cpp/actuation/ImuUncertainty.h
+++ upkie/cpp/actuation/ImuUncertainty.h
@@ -5,20 +5,27 @@
 
 #include <palimpsest/Dictionary.h>
 
+#include <Eigen/Core>
+
 #include "RobotSimulator/b3RobotSimulatorClientAPI.h"
 
-namespace upkie::cpp::actuation::bullet {
+namespace upkie::cpp::actuation {
 
 using palimpsest::Dictionary;
 
 //! Uncertainty on IMU measurements.
 struct ImuUncertainty {
-  //! Bias vector added to accelerometer measurements in the base frame.
+  //! Bias vector added to accelerometer measurements, in the IMU frame.
   Eigen::Vector3d accelerometer_bias = Eigen::Vector3d::Zero();
 
-  //! Vector of standard deviations for white noise added to accelerometer
-  // measurements in the base frame.
+  //! Standard deviations of accelerometer measurement noise, in the IMU frame.
   Eigen::Vector3d accelerometer_noise = Eigen::Vector3d::Zero();
+
+  //! Bias vector added to gyroscope measurements, in the IMU frame.
+  Eigen::Vector3d gyroscope_bias = Eigen::Vector3d::Zero();
+
+  //! Standard deviations of gyroscope measurement noise, in the IMU frame.
+  Eigen::Vector3d gyroscope_noise = Eigen::Vector3d::Zero();
 
   //! Default constructor for uncertainty initialized to default values.
   ImuUncertainty() = default;
@@ -32,7 +39,11 @@ struct ImuUncertainty {
                                                      Eigen::Vector3d::Zero());
     accelerometer_noise = config.get<Eigen::Vector3d>("accelerometer_noise",
                                                       Eigen::Vector3d::Zero());
+    gyroscope_bias =
+        config.get<Eigen::Vector3d>("gyroscope_bias", Eigen::Vector3d::Zero());
+    gyroscope_noise =
+        config.get<Eigen::Vector3d>("gyroscope_noise", Eigen::Vector3d::Zero());
   }
 };
 
-}  // namespace upkie::cpp::actuation::bullet
+}  // namespace upkie::cpp::actuation
