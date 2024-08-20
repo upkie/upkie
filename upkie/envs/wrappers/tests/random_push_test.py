70ada0f3b6d4ae5fe3356585b41f8a70bb085bb8
Tordjx <101280438+Tordjx@users.noreply.github.com>
2024-08-20 10:28:07 +0200

    Random push unit test

--- upkie/envs/wrappers/tests/random_push_test.py
+++ upkie/envs/wrappers/tests/random_push_test.py
@@ -14,27 +14,20 @@ import numpy as np
 from upkie.envs.wrappers.random_push import (
     RandomPush,
 )
-from upkie.envs.wrappers.tests.envs import ConstantObservationEnv
+from upkie.envs import UpkieGroundVelocity
 
 
 class RandomPushTestCase(unittest.TestCase):
     def test_wrapper(self):
-        env = ConstantObservationEnv(42.0)
-        wrapped_env = RandomPush(env)
+        env = UpkieGroundVelocity()
+        wrapped_env = RandomPush(env, push_prob=1, push_generator=lambda: np.array([42,42,42]))
         action = np.array([1.0])
-        observation, _, _, _, _ = wrapped_env.step(action)
-        self.assertAlmostEqual(observation[0], 42.0)
-        self.assertAlmostEqual(observation[1], 1.0)
-
-    def test_check_env(self):
-        try:
-            from stable_baselines3.common.env_checker import check_env
-
-            env = gymnasium.make("Pendulum-v1")
-            wrapped_env = AddActionToObservation(env)
-            check_env(wrapped_env)
-        except ImportError:
-            pass
+        spine_action = wrapped_env.get_spine_action(action)
+        assert "bullet" in spine_action
+        assert "external_forces" in spine_action["bullet"]
+        assert "torso" in spine_action["bullet"]["external_forces"]
+        assert "force" in spine_action["bullet"]["external_forces"]["torso"]
+        assert np.allclose(spine_action["bullet"]["external_forces"]["torso"]["force"], np.array([42,42,42]))
 
 
 if __name__ == "__main__":
