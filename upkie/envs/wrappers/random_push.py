7ad743c8fa560fb8842f7de190b81a8e2d84e8f8
Stéphane Caron <stephane.caron@inria.fr>
2024-08-27 16:39:14 +0200

    envs: Document RandomPush wrapper

--- upkie/envs/wrappers/random_push.py
+++ upkie/envs/wrappers/random_push.py
@@ -5,7 +5,7 @@
 # Copyright 2024 Inria
 
 import numpy as np
-from gymnasium import Wrapper
+from gymnasium import Env, Wrapper
 
 
 class RandomPush(Wrapper):
@@ -13,14 +13,28 @@ class RandomPush(Wrapper):
     At each step, with a given probability, apply a random push to the robot.
     """
 
-    push_prob: float
-    push_generator: callable
-    env_get_spine_action: callable
+    ## \var env
+    ## Wrapped environment.
+    env: Env
 
-    def __init__(self, env,
-                 push_prob=0.01,
-                 push_generator=lambda : np.random.normal(0, 400, 3)
-                 ):
+    # Internal reference to unwrapped get_spine_action function.
+    _env_get_spine_action: callable
+
+    ## \var push_generator
+    ## Function that generates the push force. It should return a 3D numpy
+    ## array.
+    push_generator: callable
+
+    ## \var push_prob
+    ## Probability of pushing at each step.
+    push_prob: float
+
+    def __init__(
+        self,
+        env,
+        push_prob=0.01,
+        push_generator=lambda: np.random.normal(0, 400, 3),
+    ):
         r"""!
         Initialize wrapper.
 
@@ -36,20 +50,20 @@ class RandomPush(Wrapper):
         if not hasattr(self.env, "get_spine_action"):
             raise ValueError(
                 "The environment must have a method get_spine_action"
-                )
-        self.env_get_spine_action = self.env.get_spine_action
+            )
+        self._env_get_spine_action = self.env.get_spine_action
         self.env.unwrapped.get_spine_action = self.get_spine_action
 
     def get_spine_action(self, action):
         """Adds a random push to the action."""
-        spine_action = self.env_get_spine_action(action)
+        spine_action = self._env_get_spine_action(action)
         if np.random.binomial(1, self.push_prob):
             force = self.push_generator()
-            spine_action["bullet"] = {'external_forces':
-                                      {"torso": {"force": force}}
-                                      }
-        else :  # Reset the forces to zero
-            spine_action["bullet"] = {'external_forces':
-                                      {"torso": {"force": np.zeros(3)}}
-                                      }
+            spine_action["bullet"] = {
+                "external_forces": {"torso": {"force": force}}
+            }
+        else:  # Reset the forces to zero
+            spine_action["bullet"] = {
+                "external_forces": {"torso": {"force": np.zeros(3)}}
+            }
         return spine_action
