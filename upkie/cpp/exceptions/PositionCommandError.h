ead5434a8cd29830c3805e307c0605e949032458
Stéphane Caron <stephane.caron@inria.fr>
2024-10-11 11:40:15 +0200

    Clean up unused copy constructor

--- upkie/cpp/exceptions/PositionCommandError.h
+++ upkie/cpp/exceptions/PositionCommandError.h
@@ -20,14 +20,6 @@ class PositionCommandError : public UpkieError {
   PositionCommandError(const std::string& message, int servo_id)
       : UpkieError(message) {}
 
-  /*! Copy an existing error, adding to the error message.
-   *
-   * \param[in] other Existing error.
-   * \param[in] extra_message Additional error message.
-   */
-  // PositionCommandError(const PositionCommandError& other, const std::string&
-  // extra_message) : UpkieError(other, extra_message) {}
-
   //! Empty destructor
   ~PositionCommandError() throw() {}
 };
