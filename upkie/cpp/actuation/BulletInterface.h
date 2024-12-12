83b30222d896c2e55774d671b2f8af1db229b506
Stéphane Caron <stephane.caron@inria.fr>
2024-12-13 00:48:17 +0100

    WIP: Fix robot simulator includes

--- upkie/cpp/actuation/BulletInterface.h
+++ upkie/cpp/actuation/BulletInterface.h
@@ -14,10 +14,10 @@
 
 #include "upkie/cpp/actuation/ImuUncertainty.h"
 #include "upkie/cpp/actuation/Interface.h"
-#include "upkie/cpp/actuation/RobotSimulator.h"
 #include "upkie/cpp/actuation/bullet/ContactData.h"
 #include "upkie/cpp/actuation/bullet/ExternalForce.h"
 #include "upkie/cpp/actuation/bullet/JointProperties.h"
+#include "upkie/cpp/actuation/bullet/RobotSimulator.h"
 #include "upkie/cpp/actuation/moteus/Output.h"
 #include "upkie/cpp/actuation/moteus/ServoReply.h"
 
