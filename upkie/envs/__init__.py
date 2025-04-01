3aeb961279aa769b8556445e54a004c53283a472
Stéphane Caron <stephane.caron@inria.fr>
2025-04-01 18:28:57 +0200

    minor: Refactor env wrapper keyword arguments

--- upkie/envs/__init__.py
+++ upkie/envs/__init__.py
@@ -18,23 +18,21 @@ from .upkie_servos import UpkieServos
 
 
 def make_upkie_ground_velocity(**kwargs):
-    UPKIE_SERVOS_KWARGS = (
-        "frequency",
-        "frequency_checks",
-        "init_state",
-        "regulate_frequency",
-        "shm_name",
-        "spine_config",
-    )
-    servos_kwargs = {
-        key: value
-        for key, value in kwargs.items()
-        if key in UPKIE_SERVOS_KWARGS
-    }
     ground_velocity_kwargs = {
         key: value
         for key, value in kwargs.items()
-        if key not in UPKIE_SERVOS_KWARGS
+        if key
+        in (
+            "fall_pitch",
+            "left_wheeled",
+            "max_ground_velocity",
+            "wheel_radius"
+        )
+    }
+    servos_kwargs = {
+        key: value
+        for key, value in kwargs.items()
+        if key not in ground_velocity_kwargs
     }
     env = UpkieServos(**servos_kwargs)
     return UpkieGroundVelocity(env, **ground_velocity_kwargs)
