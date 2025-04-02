ef7067f33a481c381f707c6a858b2b89e4950a38
Stéphane Caron <stephane.caron@inria.fr>
2025-04-02 14:13:47 +0200

    Update random-push wrapper unit test

--- upkie/envs/wrappers/tests/envs.py
+++ upkie/envs/wrappers/tests/envs.py
@@ -26,7 +26,14 @@ class ConstantObservationEnv(gym.Env):
         self.action_space = spaces.Box(-1.0, 1.0, shape=(1,))
         self.observation_space = spaces.Box(0.0, 2.0, shape=(1,))
         self.constant = constant
+        self.__bullet_action = {}
 
     def step(self, action):
         observation = np.array([self.constant])
         return observation, 0.0, False, False, {}
+
+    def get_bullet_action(self) -> dict:
+        return self.__bullet_action
+
+    def set_bullet_action(self, bullet_action: dict) -> None:
+        self.__bullet_action = bullet_action.copy()
