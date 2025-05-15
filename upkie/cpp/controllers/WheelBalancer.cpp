5bde1c78d3a8aac3f66cb4053f807ef042567d04
Stéphane Caron <stephane.caron@inria.fr>
2025-05-15 19:49:01 +0200

    Now Bullet balancer spine compiles

--- upkie/cpp/controllers/WheelBalancer.cpp
+++ upkie/cpp/controllers/WheelBalancer.cpp
@@ -1,63 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2022 Stéphane Caron
+// Copyright 2025 Inria
 
-#include "upkie/cpp/observers/WheelOdometry.h"
+#include "upkie/cpp/controllers/WheelBalancer.h"
 
-namespace upkie::cpp::observers {
+namespace upkie::cpp::controllers {
 
-WheelOdometry::WheelOdometry(const Parameters& params)
-    : params_(params), position_(0.0), velocity_(0.0) {}
+WheelBalancer::WheelBalancer(const Parameters& params) : params_(params) {}
 
-void WheelOdometry::reset(const Dictionary& config) {
+void WheelBalancer::reset(const Dictionary& config) {
   params_.configure(config);
-  position_ = 0.0;
-  velocity_ = 0.0;
 }
 
-void WheelOdometry::read(const Dictionary& observation) {
-  const auto& floor_contact = observation("floor_contact");
-  if (!floor_contact("contact")) {
-    return;
-  }
-  const auto& servo = observation("servo");
-  velocity_ = compute_average_velocity(floor_contact, servo);
-  position_ += velocity_ * params_.dt;
-}
+void WheelBalancer::read(const Dictionary& observation,
+                         const Dictionary& action) {}
 
-double WheelOdometry::compute_average_velocity(const Dictionary& floor_contact,
-                                               const Dictionary& servo) {
-  if (params_.signed_radius.empty()) {
-    throw std::runtime_error(
-        "[WheelOdometry] Observer not configured: 'signed_radius' is empty");
-  }
+void WheelBalancer::write(Dictionary& action) {}
 
-  double velocity_sum = 0.0;
-  unsigned nb_wheels_in_contact = 0u;
-  for (const auto& wheel_radius_pair : params_.signed_radius) {
-    const auto& wheel = wheel_radius_pair.first;
-    const bool wheel_contact = floor_contact(wheel).get<bool>("contact");
-    if (!wheel_contact) {
-      continue;
-    }
-    const double wheel_velocity = servo(wheel)("velocity");  // [rad] / [s]
-    const double signed_radius = wheel_radius_pair.second;
-    const double linear_velocity = signed_radius * wheel_velocity;  // [m] / [s]
-    velocity_sum += linear_velocity;
-    ++nb_wheels_in_contact;
-  }
-
-  if (nb_wheels_in_contact == 0u) {
-    spdlog::warn("[WheelOdometry] Contact detected, but no wheel in contact?");
-    return 0.0;
-  }
-
-  return (velocity_sum / nb_wheels_in_contact);
-}
-
-void WheelOdometry::write(Dictionary& observation) {
-  auto& output = observation(prefix());
-  output("position") = position_;
-  output("velocity") = velocity_;
-}
-
-}  // namespace upkie::cpp::observers
+}  // namespace upkie::cpp::controllers
