588364a3cc08da205990a0f9577e38003e6ebfe7
Stéphane Caron <stephane.caron@inria.fr>
2025-05-16 17:43:30 +0200

    Add wheel balancer parameters

--- upkie/cpp/controllers/WheelBalancer.h
+++ upkie/cpp/controllers/WheelBalancer.h
@@ -25,18 +25,59 @@ class WheelBalancer : public Controller {
      *
      * \param[in] config Global configuration dictionary.
      */
-    void configure(const Dictionary& config) {
-      if (!config.has("wheel_balancer")) {
+    void configure(const Dictionary& global_config) {
+      if (!global_config.has("wheel_balancer")) {
         spdlog::debug("No \"wheel_balancer\" runtime configuration");
         return;
       }
+      spdlog::info("Applying \"wheel_balancer\" runtime configuration");
+      const auto& config = global_config("wheel_balancer");
+
+      fall_pitch = config.get("fall_pitch", 1.0);
+      max_ground_velocity = config.get("max_ground_velocity", 2.0);
+      pitch_damping = config.get("pitch_damping", 0.);
+      pitch_stiffness = config.get("pitch_stiffness", 0.);
+      position_damping = config.get("position_damping", 0.);
+      position_stiffness = config.get("position_stiffness", 0.);
+      wheel_radius = config.get("wheel_radius", 0.06);
     }
 
     //! Spine timestep in [s]
     double dt;
 
     //! Fall pitch angle in [rad]
-    double fall_pitch = 1.0;
+    double fall_pitch;
+
+    //! Maximum ground velocity, in [m] / [s].
+    double max_ground_velocity;
+
+    /*! Pitch error (normalized) damping gain.
+     *
+     * Corresponds to the proportional term of the velocity PI controller,
+     * equivalent to the derivative term of the acceleration PD controller.
+     */
+    double pitch_damping;
+
+    /*! Pitch error (normalized) stiffness gain.
+     *
+     * Corresponds to the integral term of the velocity PI controller,
+     * equivalent to the proportional term of the acceleration PD controller.
+     */
+    double pitch_stiffness;
+
+    /*! Position error (normalized) damping gain.
+     *
+     * Corresponds to the proportional term of the velocity PI controller,
+     * equivalent to the derivative term of the acceleration PD controller.
+     */
+    double position_damping;
+
+    /*! Position error (normalized) stiffness gain.
+     *
+     * Corresponds to the integral term of the velocity PI controller,
+     * equivalent to the proportional term of the acceleration PD controller.
+     */
+    double position_stiffness;
 
     //! Wheel radius in [m]
     double wheel_radius;
@@ -74,8 +115,14 @@ class WheelBalancer : public Controller {
   //! Controller parameters.
   Parameters params_;
 
-  //! Ground velocity offset used for balancing, in [m] / [s]
+  //! Ground velocity offset used for balancing, in [m] / [s].
   double ground_velocity_;
+
+  //! Ground velocity offset due to the integral error, in [m] / [s].
+  double integral_velocity_;
+
+  //! Target ground position in [m].
+  double target_ground_position_;
 };
 
 }  // namespace upkie::cpp::controllers
