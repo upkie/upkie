f9bd6c3987b275a945218c483d048dc5950a6789
Stéphane Caron <stephane.caron@inria.fr>
2025-05-23 00:43:35 +0200

    WIP: Environment base class palinode

---
+++ upkie/envs/upkie_base.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Inria
+
+import abc
+from typing import Optional, Tuple
+
+import gymnasium as gym
+import numpy as np
+from loop_rate_limiters import RateLimiter
+
+from upkie.exceptions import UpkieException
+from upkie.model import Model
+from upkie.utils.robot_state import RobotState
+
+
+class UpkieBase(gym.Env, abc.ABC):
+    r"""!
+    Base class with features shared by all Upkie environments.
+
+    Those features are:
+
+    - Loop frequency regulation
+    - Initial state randomization
+    """
+
+    __frequency: Optional[float]
+    __rate: Optional[RateLimiter]
+    __regulate_frequency: bool
+
+    ## \var init_state
+    ## Initial state for the floating base of the robot, which may be
+    ## randomized upon resets.
+    init_state: RobotState
+
+    ## \var model
+    ## Robot model read from its URDF description.
+    model: Model
+
+    def __init__(
+        self,
+        frequency: Optional[float] = 200.0,
+        frequency_checks: bool = True,
+        init_state: Optional[RobotState] = None,
+        regulate_frequency: bool = True,
+    ) -> None:
+        r"""!
+        Initialize environment.
+
+        \param frequency Regulated frequency of the control loop, in Hz. Can be
+            prescribed even when `regulate_frequency` is unset, in which case
+            `self.dt` will be defined but the loop frequency will not be
+            regulated.
+        \param frequency_checks If `regulate_frequency` is set and this
+            parameter is true (default), a warning is issued every time the
+            control loop runs slower than the desired `frequency`. Set this
+            parameter to false to disable these warnings.
+        \param init_state Initial state of the robot, only used in simulation.
+        \param regulate_frequency If set (default), the environment will
+            regulate the control loop frequency to the value prescribed in
+            `frequency`.
+
+        \throw SpineError If the spine did not respond after the prescribed
+            number of trials.
+        """
+        if regulate_frequency and frequency is None:
+            raise UpkieException(f"{regulate_frequency=} but {frequency=}")
+        if init_state is None:
+            init_state = RobotState(
+                position_base_in_world=np.array([0.0, 0.0, 0.6])
+            )
+
+        # Class attributes
+        self.__frequency = frequency
+        self.__frequency_checks = frequency_checks
+        self.__rate = None
+        self.__regulate_frequency = regulate_frequency
+        self.init_state = init_state
+
+    @property
+    def dt(self) -> Optional[float]:
+        """!
+        Regulated period of the control loop in seconds, or `None` if there
+        is no loop frequency regulation.
+        """
+        return 1.0 / self.__frequency if self.__frequency is not None else None
+
+    @property
+    def frequency(self) -> Optional[float]:
+        """!
+        Regulated frequency of the control loop in Hz, or `None` if there is
+        no loop frequency regulation.
+        """
+        return self.__frequency
+
+    def get_neutral_action(self) -> dict:
+        r"""!
+        Get the neutral action where servos don't move.
+
+        \return Neutral action where servos don't move.
+        """
+        return self.__neutral_action.copy()
+
+    @abc.abstractmethod
+    def log(self, name: str, entry) -> None:
+        r"""!
+        Log a new entry to the spine, if one is connected.
+
+        \param name Name of the entry.
+        \param entry Dictionary to log along with the actual action.
+        """
+
+    def reset(
+        self,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+    ) -> Tuple[dict, dict]:
+        r"""!
+        Resets the spine and get an initial observation.
+
+        \param seed Number used to initialize the environment’s internal random
+            number generator.
+        \param options Currently unused.
+        \return
+            - `observation`: Initial vectorized observation, i.e. an element
+              of the environment's `observation_space`.
+            - `info`: Dictionary with auxiliary diagnostic information. For
+              Upkie this is the full observation dictionary sent by the spine.
+        """
+        super().reset(seed=seed)
+        if self.__regulate_frequency:
+            rate_name = f"{self.__class__.__name__} rate limiter"
+            self.__rate = RateLimiter(
+                self.__frequency,
+                name=rate_name,
+                warn=self.__frequency_checks,
+            )
+        return {}, {}
+
+    def step(self) -> None:
+        r"""!
+        Regulate the control loop frequency, if applicable.
+        """
+        if self.__regulate_frequency:
+            self.__rate.sleep()  # wait until clock tick to send the action
+            self.log("rate", {"slack": self.__rate.slack})
+
+    def update_init_rand(self, **kwargs) -> None:
+        r"""!
+        Update initial-state randomization.
+
+        Keyword arguments are forwarded as is to \ref
+        upkie.utils.robot_state_randomization.RobotStateRandomization.update.
+        """
+        self.init_state.randomization.update(**kwargs)
