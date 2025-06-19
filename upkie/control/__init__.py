a0e6a9e493bf10e01d0dbea4a9af314deb44e572
Stéphane Caron <stephane.caron@inria.fr>
2025-06-19 19:53:40 +0200

    Add init file to upkie.control submodule

---
+++ upkie/control/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 Inria
+#
+## \namespace upkie.control
+## \brief Functions and classes related to balance control.
+
+from .mpc_balancer import MPCBalancer
+
+__all__ = [
+    "MPCBalancer",
+]
