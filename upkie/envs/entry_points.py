0340db0426c5b6c90f8604456ff8498db1412e34
Stéphane Caron <stephane.caron@inria.fr>
2025-07-31 00:24:48 +0200

    Add upkie.envs.pipelines submodule

---
+++ upkie/envs/entry_points.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 Inria
+
+from upkie.envs.pipelines import ServoPipeline
+from upkie.envs.wrappers import Pendulum
+
+from .upkie_mock_env import UpkieMockEnv
+from .upkie_spine_env import UpkieSpineEnv
+
+
+def wrap_pendulum(EnvClass, **kwargs):
+    r"""!
+    Make a new ground-velocity environment around an Upkie environment.
+
+    This function is meant to be called by `gymnasium.make()` rather than to be
+    called directly.
+
+    \param kwargs Keyword arguments forwarded to both the
+        \ref upkie.envs.wrappers.pendulum.Pendulum wrapper and the internal
+        Upkie environment.
+    \return New ground-velocity environment.
+    """
+    pendulum_keys = {
+        "fall_pitch",
+        "left_wheeled",
+        "max_ground_velocity",
+        "wheel_radius",
+    }
+    pendulum_kwargs = {
+        key: value for key, value in kwargs.items() if key in pendulum_keys
+    }
+    servo_keys = {"max_gain_scale"}
+    servo_kwargs = {
+        key: value for key, value in kwargs.items() if key in servo_keys
+    }
+    env_kwargs = {
+        key: value
+        for key, value in kwargs.items()
+        if key not in pendulum_keys and key not in servo_keys
+    }
+    servo_pipeline = ServoPipeline(**servo_kwargs)
+    env = EnvClass(servo_pipeline, **env_kwargs)
+    return Pendulum(env, **pendulum_kwargs)
+
+
+def wrap_mock_pendulum(**kwargs):
+    return wrap_pendulum(UpkieMockEnv, **kwargs)
+
+
+def wrap_spine_pendulum(**kwargs):
+    return wrap_pendulum(UpkieSpineEnv, **kwargs)
---
+++ upkie/envs/pipelines/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 Inria
+
+from .pipeline import Pipeline
+from .servo_pipeline import ServoPipeline
+
+__all__ = [
+    "Pipeline",
+    "ServoPipeline",
+]
