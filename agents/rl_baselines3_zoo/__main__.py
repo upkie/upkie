620bc312bfa3482d45e74584b8e52defa366f2b2
Stéphane Caron <stephane.caron@inria.fr>
2025-08-06 10:21:18 +0200

    WIP: Train a new policy using the RL Baselines3 Zoo

---
+++ agents/rl_baselines3_zoo/__main__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 Inria
+
+import sys
+from pathlib import Path
+
+from rl_zoo3.train import train as rl_zoo3_train
+
+import upkie.envs
+
+if __name__ == "__main__":
+    upkie.envs.register()
+
+    custom_args = (
+        ["--env", "Upkie-PyBullet-Pendulum"],
+        ["--algo", "ppo"],
+        ["--eval-freq", "-1"],
+        ["--env-kwargs", "gui:False", "regulate_frequency:False"],
+        ["--tensorboard-log", "./logs/tensorboard/"],
+    )
+
+    for args in custom_args:
+        key = args[0]
+        if key not in sys.argv:
+            sys.argv.extend(args)
+
+    # Use custom hyperparameters file if not specified
+    if "--conf" not in sys.argv and "--conf-file" not in sys.argv:
+        hyperparams_file = Path(__file__).parent / "hyperparams.yml"
+        sys.argv.extend(["--conf-file", str(hyperparams_file)])
+
+    # Call the rl_zoo3 train function which handles all argument parsing
+    rl_zoo3_train()
