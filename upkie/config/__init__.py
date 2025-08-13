f011b0477340fdb14be936cfc27f37d44b365d40
Stéphane Caron <stephane.caron@inria.fr>
2025-08-13 13:09:17 +0200

    WIP: Add robot config to upkie.config

--- upkie/config/__init__.py
+++ upkie/config/__init__.py
@@ -8,11 +8,13 @@
 ## \brief Static robot configuration.
 
 from .bullet_config import BULLET_CONFIG
+from .robot_config import ROBOT_CONFIG
 from .spine_config import SPINE_CONFIG
 from .user_config import USER_CONFIG
 
 __all__ = [
     "BULLET_CONFIG",
+    "ROBOT_CONFIG",
     "SPINE_CONFIG",
     "USER_CONFIG",
 ]
