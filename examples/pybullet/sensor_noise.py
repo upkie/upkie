d0d9e86130abc29c0086382f9a81f125dd57ef20
Stéphane Caron <stephane.caron@inria.fr>
2025-09-09 09:52:20 +0200

    Update sensor noise example to PyBullet env

--- examples/pybullet/sensor_noise.py
+++ examples/pybullet/sensor_noise.py
@@ -25,25 +25,16 @@ JOINT_NOISE = {
 if __name__ == "__main__":
     upkie.envs.register()
     with gym.make(
-        "Upkie-Spine-Pendulum",
+        "Upkie-PyBullet-Pendulum",
         frequency=200.0,
-        spine_config={
-            "bullet": {
-                "imu_uncertainty": {
-                    "accelerometer_bias": [1e-3, 1e-3, 5e-3],
-                    "accelerometer_noise": 0.2,
-                    # Try increasing noise or y-axis bias: it affects balancing
-                    "gyroscope_bias": [0.0, 2e-1, 0.0],
-                    "gyroscope_noise": 0.05,
-                },
-                "joint_properties": {
-                    "left_hip": JOINT_NOISE,
-                    "left_knee": JOINT_NOISE,
-                    "left_wheel": JOINT_NOISE,
-                    "right_hip": JOINT_NOISE,
-                    "right_knee": JOINT_NOISE,
-                    "right_wheel": JOINT_NOISE,
-                },
+        bullet_config={
+            "joint_properties": {
+                "left_hip": JOINT_NOISE,
+                "left_knee": JOINT_NOISE,
+                "left_wheel": JOINT_NOISE,
+                "right_hip": JOINT_NOISE,
+                "right_knee": JOINT_NOISE,
+                "right_wheel": JOINT_NOISE,
             }
         },
     ) as env:
@@ -54,7 +45,10 @@ if __name__ == "__main__":
             action = gain.dot(observation).reshape((1,))
             if step == 100:  # disturb the initial equilibrium
                 action = env.action_space.sample()
-            observation, reward, terminated, truncated, _ = env.step(action)
+            observation, reward, terminated, truncated, info = env.step(action)
+            left_knee_torque = info["spine_observation"]["servo"]["left_knee"][
+                "torque"
+            ]
+            print(f"{left_knee_torque=}")
             if terminated or truncated:
-                observation, _ = env.reset()
                 observation, _ = env.reset()
