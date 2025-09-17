e77da285701e2e3d5f54b27ee8f6f9bdd79b4f70
Stéphane Caron <stephane.caron@inria.fr>
2025-09-17 12:09:40 +0200

    Don't use dataclass decorator in PointContact either

--- upkie/utils/point_contact.py
+++ upkie/utils/point_contact.py
@@ -5,16 +5,17 @@
 
 """Point contact data class for robot physics simulation."""
 
-from dataclasses import dataclass
-
 import numpy as np
 
 
-@dataclass
 class PointContact:
     """!
     Contact information from the robot's perspective.
     """
+
+    ## \var force_in_world
+    ## Total contact force vector in world coordinates.
+    force_in_world: np.ndarray
 
     ## \var link_name
     ## Name of the robot link in contact.
@@ -24,6 +25,33 @@ class PointContact:
     ## Position of the contact point in world coordinates.
     position_contact_in_world: np.ndarray
 
-    ## \var force_in_world
-    ## Total contact force vector in world coordinates.
-    force_in_world: np.ndarray
+    def __init__(
+        self,
+        link_name: str,
+        position_contact_in_world: np.ndarray,
+        force_in_world: np.ndarray,
+    ):
+        r"""!
+        Initialize point contact.
+
+        \param force_in_world Contact force vector in the world frame.
+        \param link_name Name of the robot link in contact.
+        \param position_contact_in_world Position of the contact point in the
+            world frame.
+        """
+        self.force_in_world = force_in_world
+        self.link_name = link_name
+        self.position_contact_in_world = position_contact_in_world
+
+    def __repr__(self) -> str:
+        """!
+        String representation of the point contact.
+
+        \return String representation showing all contact information.
+        """
+        return (
+            f"PointContact("
+            f"link_name='{self.link_name}', "
+            f"position_contact_in_world={self.position_contact_in_world.tolist()}, "
+            f"force_in_world={self.force_in_world.tolist()})"
+        )
