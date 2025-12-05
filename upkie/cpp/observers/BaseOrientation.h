0ad0d5d9bed77036e9dafbee71272d14ebfcbc04
Stéphane Caron <stephane.caron@inria.fr>
2025-12-05 17:05:18 +0100

    Revert "Changing IMU default orientation"
    This reverts commit 31d5ed0c87dbd243e02935bf27cad10fa97a1df8.

--- upkie/cpp/observers/BaseOrientation.h
+++ upkie/cpp/observers/BaseOrientation.h
@@ -159,9 +159,8 @@ class BaseOrientation : public Observer {
      */
     void configure(const Dictionary& config) {
       // Default IMU and ARS orientations
-      rotation_base_to_imu << 0.0, -1.0, 0.0,
-                              0.0, 0.0, -1.0,
-                              1.0, 0.0, 0.0;
+      rotation_base_to_imu.setZero();
+      rotation_base_to_imu.diagonal() << -1.0, 1.0, -1.0;
       rotation_ars_to_world.setZero();
       rotation_ars_to_world.diagonal() << 1.0, -1.0, -1.0;
 
