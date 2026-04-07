d37cf6b5ccb084372b8a572b111bd9683ab9806b
Stéphane Caron <stephane.caron@normalesup.org>
2026-04-07 09:55:31 +0200

    Expose new classes in upkie.model submodule

--- upkie/model/__init__.py
+++ upkie/model/__init__.py
@@ -9,11 +9,18 @@
 from .joint import Joint
 from .joint_limit import JointLimit
 from .joint_properties import JointProperties
+from .link import CollisionGeometry, Link
 from .model import Model
+from .se3 import SE3
+from .kinematic_tree  import KinematicTree
 
 __all__ = [
+    "CollisionGeometry",
     "Joint",
     "JointLimit",
     "JointProperties",
+    "KinematicTree",
+    "Link",
     "Model",
+    "SE3",
 ]
